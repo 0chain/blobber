5af2fcb71b53ed915c64ccf56b3f94bd5cc804ae
Nidhi Sharma <nidhi.ardent@gmail.com>
2018-08-14 10:40:54 -0700

    Making sure docker build takes the latest local code

--- docker.local/bin/build.blobber.sh
+++ docker.local/bin/build.blobber.sh
@@ -2,5 +2,5 @@
 
 for i in $(seq 1 1);
 do
-  BLOBBER=$i docker-compose -p blobber$i -f docker.local/docker-compose.yml build --force-rm
+  BLOBBER=$i docker-compose -p blobber$BLOBBER_ID -f docker.local/docker-compose.yml build --force-rm
 done
