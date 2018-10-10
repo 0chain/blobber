cf143965ac072ec39ad72e927259be9c4843be15
Ameen <nameen@0chain.net>
2018-10-10 01:02:01 -0700

    Sending register txn to all miners. No response and timeout.

--- code/go/src/0chain.net/blobber/protocol.go
+++ code/go/src/0chain.net/blobber/protocol.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"time"
 	"bytes"
+	"context"
 	"io/ioutil"
 	"encoding/json"
 	"net/http"
 
 	"0chain.net/chain"
+	"0chain.net/common"
 	"0chain.net/encryption"
+	"0chain.net/transaction"
 	"0chain.net/config"
 	. "0chain.net/logging"
 	"0chain.net/node"
@@ -21,13 +24,9 @@ const MAX_REGISTRATION_RETRIES = 3
 // TODO: (0) Fix hardcoding
 const STORAGE_CONTRACT_ADDRESS 	= "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d7"
 const PUT_TRANSACTION 			= "v1/transaction/put"
+var   REGISTRATION_DATA			= json.RawMessage(`{"add_blobber": ""}`)
+const SMART_CONTRACT_TYPE		= 1000
 
-type Transaction struct {
-	ClientID  			string 				`json:"client_id,omitempty"`
-	PublicKey 			string        		`json:"public_key,omitempty"`
-	ToClientID      	string    		 	`json:"to_client_id,omitempty"`
-	ChainID         	string    			`json:"chain_id,omitempty"`
-}
 
 // Storage smart contract 
 type StorageNode struct {
@@ -65,52 +64,34 @@ func SetupProtocol(c *chain.Chain) {
 
 func (sp *StorageProtocolImpl) Register() {
 	if (sp.ServerChain != nil) {	
-		// Get a random miner
-		miner := sp.ServerChain.Miners.GetRandomNode()
-		url := fmt.Sprintf("%v/%v", miner.GetURLBase(), PUT_TRANSACTION);
-		// url := fmt.Sprintf("http://localhost:7071/%v", PUT_TRANSACTION);
-		txn := Transaction {
+
+		txn := transaction.Transaction {
+			Version 	: 	transaction.TRANSACTION_VERION,
 			ClientID 	:	node.Self.ID,
 			PublicKey 	: 	encryption.Hash(node.Self.ID),
 			ToClientID 	:	STORAGE_CONTRACT_ADDRESS,
 			ChainID 	: 	config.Configuration.ChainID,
+			Value		: 	0,
+			Data		:   REGISTRATION_DATA,
+			TxType 		:   SMART_CONTRACT_TYPE,
+			CreationDate: 	common.Now(),
 		}
-		// TODO: Add Signature..
-		fmt.Println(txn)
-		jsObj, err := json.Marshal(txn)
+		hashdata := fmt.Sprintf("%v:%v:%v:%v:%v", txn.CreationDate, txn.ClientID, 
+					txn.ToClientID, txn.Value, encryption.Hash(string(txn.Data)))
+		var err error
+		txn.Signature, err = node.Self.Sign(hashdata)
 		if (err != nil) {
-			fmt.Println("Error:" , err)
+			Logger.Info("Signing Failed")
 		}
-		fmt.Println("JSON:", jsObj)
-		Logger.Info("Registering to Miner", zap.String("ID", miner.ID), zap.String("URL", url))
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsObj))
-	   	req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Access-Control-Allow-Origin", "*")		
-		tr := &http.Transport{
-			ResponseHeaderTimeout: 3 * time.Second,
-		}
-		var resp *http.Response
-		client := &http.Client{Transport: tr}
-		for i:= 0; i < MAX_REGISTRATION_RETRIES; i++ {
-			resp, err = client.Do(req)
-			if err == nil {
-			    break;
-			}
-			Logger.Error("Register error", zap.String("error", err.Error()))
-		}
-
-		if (err == nil) {
-			defer resp.Body.Close()
-			fmt.Println("response Status:", resp.Status)
-			fmt.Println("response Headers:", resp.Header)
-			body, _ := ioutil.ReadAll(resp.Body)
-			fmt.Println("response Body:", string(body))			
-			return
-		}
-		Logger.Error("Failed after ", zap.Int("retried", MAX_REGISTRATION_RETRIES))
-		// TODO: Handle error?
+		// Get miners
+		miners := sp.ServerChain.Miners.GetRandomNodes(sp.ServerChain.Miners.Size())
+		for _, miner := range miners {
+			url := fmt.Sprintf("%v/%v", miner.GetURLBase(), PUT_TRANSACTION);
+			go sendTransaction(url, txn)
+    	}
 	}
 }
+
 
 func (sp *StorageProtocolImpl) VerifyAllocationTransaction() {
 
@@ -131,3 +112,42 @@ func (sp *StorageProtocolImpl) CollectMarker() {
 func (sp *StorageProtocolImpl) RedeemMarker() {
 
 }
+
+/*============================ Private functions =======================*/
+func sendTransaction(url string, txn transaction.Transaction) {
+	jsObj, err := json.Marshal(txn)
+	if (err != nil) {
+		fmt.Println("Error:" , err)
+	}
+	fmt.Println("TXN:", txn)
+	req, ctx, cncl, err := newHttpRequest(http.MethodPost, url, jsObj)
+	defer cncl()
+	var resp *http.Response
+	for i:= 0; i < MAX_REGISTRATION_RETRIES; i++ {
+		resp, err = http.DefaultClient.Do(req.WithContext(ctx))
+		if err == nil {
+		    break;
+		}
+		//TODO: Handle ctx cncl
+		Logger.Error("Register", zap.String("error", err.Error()), zap.String("URL", url))
+	}
+
+	if (err == nil) {
+		defer resp.Body.Close()
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("response Body:", string(body))			
+		return
+	}
+	Logger.Error("Failed after ", zap.Int("retried", MAX_REGISTRATION_RETRIES))
+}
+
+func newHttpRequest(method string, url string, data []byte) (*http.Request, context.Context, context.CancelFunc, error) {
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
+   	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Access-Control-Allow-Origin", "*")
+	ctx, cncl := context.WithTimeout(context.Background(), time.Second*10)
+	return req, ctx, cncl, err
+}
+
--- code/go/src/0chain.net/transaction/entity.go
+++ code/go/src/0chain.net/transaction/entity.go
@@ -1,25 +1,24 @@
 package transaction
 
 import (
+	"encoding/json"
+	
 	"0chain.net/common"
 )
 
+const TRANSACTION_VERION 	=	"1.0"
+
 //Transaction entity that encapsulates the transaction related data and meta data
 type Transaction struct {
-	Hash      string
-	Version   string
-	ClientID  string
-	PublicKey string
-
-	ToClientID      string
-	ChainID         string
-	TransactionData string
-	Value           int64
-	Signature       string
-	CreationDate    common.Timestamp
-
-	TransactionType int
-
-	// a parent transaction introdcues certain state and this state is managed as new transactions are created referencing to this parent transaction
-	ParentTransactionHash string
+	Hash 				string 				`json:"hash,omitempty"`
+	Version   			string 				`json:"version,omitempty"`
+	ClientID  			string 				`json:"client_id,omitempty"`
+	PublicKey 			string        		`json:"public_key,omitempty"`
+	ToClientID      	string    		 	`json:"to_client_id,omitempty"`
+	ChainID         	string    			`json:"chain_id,omitempty"`
+	Value 				int64 				`json:"transaction_value,omitempty"`
+	Data 				json.RawMessage		`json:"transaction_data",omitempty"`
+	TxType 				int 				`json:"transaction_type",omitempty"`
+	CreationDate 		common.Timestamp    `json:"creation_date",omitempty"`
+	Signature 			string 				`json:"signature",omitempty"`
 }
