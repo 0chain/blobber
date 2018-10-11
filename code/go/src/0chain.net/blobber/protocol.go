d3026ed79c23cf369ddafd79fd7ef61fb7dc38d6
Ameen <nameen@0chain.net>
2018-10-10 22:34:02 -0700

    Fixed sending correct transaction data on blobber registration. Failing on self sign

--- code/go/src/0chain.net/blobber/protocol.go
+++ code/go/src/0chain.net/blobber/protocol.go
@@ -13,7 +13,6 @@ import (
 	"0chain.net/common"
 	"0chain.net/encryption"
 	"0chain.net/transaction"
-	"0chain.net/config"
 	. "0chain.net/logging"
 	"0chain.net/node"
 	"go.uber.org/zap"
@@ -24,16 +23,17 @@ const MAX_REGISTRATION_RETRIES = 3
 // TODO: (0) Fix hardcoding
 const STORAGE_CONTRACT_ADDRESS 	= "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d7"
 const PUT_TRANSACTION 			= "v1/transaction/put"
-var   REGISTRATION_DATA			= json.RawMessage(`{"add_blobber": ""}`)
 const SMART_CONTRACT_TYPE		= 1000
 
 
 // Storage smart contract 
-type StorageNode struct {
-	ID        string `json:"id"`
-	BaseURL   string `json:"url"`
-	PublicKey string `json:"-"`
+type StorageData struct {
+	Name 	  string 	`json:"name"`
+	ID        string 	`json:"id"`
+	BaseURL   string 	`json:"url"`
 }
+
+
 
 //StorageProtocol - interface for the storage protocol
 type StorageProtocol interface {
@@ -70,12 +70,11 @@ func (sp *StorageProtocolImpl) Register() {
 			ClientID 	:	node.Self.ID,
 			PublicKey 	: 	encryption.Hash(node.Self.ID),
 			ToClientID 	:	STORAGE_CONTRACT_ADDRESS,
-			ChainID 	: 	config.Configuration.ChainID,
 			Value		: 	0,
-			Data		:   REGISTRATION_DATA,
 			TxType 		:   SMART_CONTRACT_TYPE,
 			CreationDate: 	common.Now(),
 		}
+		txn.Data = fmt.Sprintf("{\"name\":\"add_blobber\",\"input\":{\"id\":\"%v\",\"url\":\"%v\"", node.Self.GetKey(),node.Self.GetURLBase())
 		hashdata := fmt.Sprintf("%v:%v:%v:%v:%v", txn.CreationDate, txn.ClientID, 
 					txn.ToClientID, txn.Value, encryption.Hash(string(txn.Data)))
 		var err error
@@ -119,7 +118,6 @@ func sendTransaction(url string, txn transaction.Transaction) {
 	if (err != nil) {
 		fmt.Println("Error:" , err)
 	}
-	fmt.Println("TXN:", txn)
 	req, ctx, cncl, err := newHttpRequest(http.MethodPost, url, jsObj)
 	defer cncl()
 	var resp *http.Response
