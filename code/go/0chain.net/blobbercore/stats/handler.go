3926143c87f4ceaacb8a53e0973eaae62064fdad
Ameen <nameen@0chain.net>
2019-10-01 23:12:15 -0700

    REST API getstats and getstats?allocation=

--- code/go/0chain.net/blobbercore/stats/handler.go
+++ code/go/0chain.net/blobbercore/stats/handler.go
@@ -1,11 +1,14 @@
 package stats
 
 import (
+	"context"
 	"html/template"
 	"net/http"
 
-  "0chain.net/blobbercore/datastore"
-  . "0chain.net/core/logging"
+	"0chain.net/blobbercore/constants"
+	"0chain.net/blobbercore/datastore"
+	"0chain.net/core/common"
+	. "0chain.net/core/logging"
 
 	"go.uber.org/zap"
 )
@@ -100,7 +103,7 @@ const tpl = `<!DOCTYPE html>
         <td>Redeemed Challenges</td>
       </tr>
       {{range .AllocationStats}}
-      
+
       <tr>
         <td>{{ .AllocationID }}</td>
         <td>{{ .UsedSize }}</td>
@@ -123,12 +126,31 @@ const tpl = `<!DOCTYPE html>
 
 func StatsHandler(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.New("diagnostics").Funcs(funcMap).Parse(tpl))
-  ctx := datastore.GetStore().CreateTransaction(r.Context())
-  db := datastore.GetStore().GetTransaction(ctx)
+	ctx := datastore.GetStore().CreateTransaction(r.Context())
+	db := datastore.GetStore().GetTransaction(ctx)
 	defer db.Rollback()
 	bs := LoadBlobberStats(ctx)
 	err := t.Execute(w, bs)
 	if err != nil {
 		Logger.Error("Error in executing the template", zap.Error(err))
 	}
+}
+
+func GetStatsHandler(ctx context.Context, r *http.Request) (interface{}, error) {
+	q := r.URL.Query()
+	ctx = context.WithValue(ctx, constants.ALLOCATION_CONTEXT_KEY, q.Get("allocation"))
+	ctx = datastore.GetStore().CreateTransaction(ctx)
+	db := datastore.GetStore().GetTransaction(ctx)
+	defer db.Rollback()
+	bs := LoadBlobberStats(ctx)
+	allocationID := ctx.Value(constants.ALLOCATION_CONTEXT_KEY).(string)
+	if len(allocationID) <= 0 {
+		return bs, nil
+	}
+	for _, allocStat := range bs.AllocationStats {
+		if allocStat.AllocationID == allocationID {
+			return allocStat, nil
+		}
+	}
+	return nil, common.NewError("allocation_stats_not_found", "Stats for allocation not found")
 }
