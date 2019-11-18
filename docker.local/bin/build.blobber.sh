b69bb915e351d7cfce22fad1f934e6cafe0e292f
Sarath Ambati <sarath@0chain.net>
2019-11-18 15:03:46 -0800

    improve the docker build performance for blobber

--- docker.local/bin/build.blobber.sh
+++ docker.local/bin/build.blobber.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -e
+
+GIT_COMMIT=$(git rev-list -1 HEAD)
+echo $GIT_COMMIT
+
+docker build --build-arg GIT_COMMIT=$GIT_COMMIT -f docker.local/ValidatorDockerfile . -t validator
+docker build --build-arg GIT_COMMIT=$GIT_COMMIT -f docker.local/Dockerfile . -t blobber
 
 for i in $(seq 1 6);
 do
