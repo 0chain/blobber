3116543fd7388985b38071bda5af3fb23fa7c2a4
Krishna <kupadhyaya@techracers.io>
2020-05-27 15:10:17 +0530

    Add json tag to time fields

--- code/go/0chain.net/blobbercore/datastore/model.go
+++ code/go/0chain.net/blobbercore/datastore/model.go
@@ -3,6 +3,6 @@ package datastore
 import "time"
 
 type ModelWithTS struct {
-	CreatedAt time.Time `gorm:"column:created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at"`
+	CreatedAt time.Time `gorm:"column:created_at"  json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at"  json:"updated_at"`
 }
