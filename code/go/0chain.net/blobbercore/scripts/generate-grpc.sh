ff335cff1b7ceaa59878d2d66b494223e69b66d3
Shravan Shetty <shravanshetty322@gmail.com>
2021-03-26 20:53:23 +0530

    :sparkles: added openapi spec for browser clients

--- code/go/0chain.net/blobbercore/scripts/generate-grpc.sh
+++ code/go/0chain.net/blobbercore/scripts/generate-grpc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-protoc -I ./blobbergrpc/proto --go-grpc_out=. --go_out=. --grpc-gateway_out=. ./blobbergrpc/proto/blobber.proto
+protoc -I ./blobbergrpc/proto --go-grpc_out=. --go_out=. --grpc-gateway_out=. --openapiv2_out=./openapi ./blobbergrpc/proto/blobber.proto
