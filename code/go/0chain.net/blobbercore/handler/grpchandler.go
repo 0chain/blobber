c8af1c725de96ff67aa7c09faa7633bff0b50b1d
Shravan Shetty <shravanshetty322@gmail.com>
2021-03-31 21:20:36 +0530

    :sparkles: migrated file meta data api to grpc

--- code/go/0chain.net/blobbercore/handler/grpchandler.go
+++ code/go/0chain.net/blobbercore/handler/grpchandler.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"context"
 
+	"0chain.net/blobbercore/reference"
+
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 
 	"0chain.net/core/common"
@@ -71,7 +73,7 @@ func RegisterGRPCServices(r *mux.Router, server *grpc.Server) {
 	mux := runtime.NewServeMux()
 	blobbergrpc.RegisterBlobberServer(server, blobberService)
 	blobbergrpc.RegisterBlobberHandlerServer(context.Background(), mux, blobberService)
-	r.PathPrefix("/v2").Handler(mux)
+	r.PathPrefix("/").Handler(mux)
 }
 
 func newGRPCBlobberService() *blobberGRPCService {
@@ -87,4 +89,107 @@ func (b *blobberGRPCService) GetAllocation(ctx context.Context, request *blobber
 	}
 
 	return &blobbergrpc.GetAllocationResponse{Allocation: convertAllocationToGRPCAllocation(allocation)}, nil
+}
+
+func (b *blobberGRPCService) GetFileMetaData(ctx context.Context, req *blobbergrpc.GetFileMetaDataRequest) (*blobbergrpc.GetFileMetaDataResponse, error) {
+	logger := ctxzap.Extract(ctx)
+	allocationObj, err := b.storageHandler.verifyAllocation(ctx, req.Allocation, true)
+	if err != nil {
+		return nil, common.NewError("invalid_parameters", "Invalid allocation id passed."+err.Error())
+	}
+	allocationID := allocationObj.ID
+
+	clientID := req.Context.Client
+	if len(clientID) == 0 {
+		return nil, common.NewError("invalid_operation", "Operation needs to be performed by the owner of the allocation")
+	}
+
+	path_hash := req.PathHash
+	path := req.Path
+	if len(path_hash) == 0 {
+		if len(path) == 0 {
+			return nil, common.NewError("invalid_parameters", "Invalid path")
+		}
+		path_hash = reference.GetReferenceLookup(allocationID, path)
+	}
+
+	fileref, err := reference.GetReferenceFromLookupHash(ctx, allocationID, path_hash)
+	if err != nil {
+		return nil, common.NewError("invalid_parameters", "Invalid file path. "+err.Error())
+	}
+
+	if fileref.Type != reference.FILE {
+		return nil, common.NewError("invalid_parameters", "Path is not a file.")
+	}
+
+	commitMetaTxns, err := reference.GetCommitMetaTxns(ctx, fileref.ID)
+	if err != nil {
+		logger.Error("Failed to get commitMetaTxns from refID", zap.Error(err), zap.Any("ref_id", fileref.ID))
+	}
+
+	collaborators, err := reference.GetCollaborators(ctx, fileref.ID)
+	if err != nil {
+		logger.Error("Failed to get collaborators from refID", zap.Error(err), zap.Any("ref_id", fileref.ID))
+	}
+
+	authTokenString := req.AuthToken
+
+	if (allocationObj.OwnerID != clientID &&
+		allocationObj.PayerID != clientID &&
+		!reference.IsACollaborator(ctx, fileref.ID, clientID)) || len(authTokenString) > 0 {
+		authTicketVerified, err := b.storageHandler.verifyAuthTicket(ctx, req.AuthToken, allocationObj, fileref, clientID)
+		if err != nil {
+			return nil, err
+		}
+		if !authTicketVerified {
+			return nil, common.NewError("auth_ticket_verification_failed", "Could not verify the auth ticket.")
+		}
+		fileref.Path = ""
+	}
+
+	var commitMetaTxnsGRPC []*blobbergrpc.CommitMetaTxn
+	for _, c := range commitMetaTxns {
+		commitMetaTxnsGRPC = append(commitMetaTxnsGRPC, &blobbergrpc.CommitMetaTxn{
+			RefId:     c.RefID,
+			TxnId:     c.TxnID,
+			CreatedAt: c.CreatedAt.UnixNano(),
+		})
+	}
+
+	var collaboratorsGRPC []*blobbergrpc.Collaborator
+	for _, c := range collaborators {
+		collaboratorsGRPC = append(collaboratorsGRPC, &blobbergrpc.Collaborator{
+			RefId:     c.RefID,
+			ClientId:  c.ClientID,
+			CreatedAt: c.CreatedAt.UnixNano(),
+		})
+	}
+
+	return &blobbergrpc.GetFileMetaDataResponse{
+		Type:                fileref.Type,
+		LookupHash:          fileref.LookupHash,
+		Name:                fileref.Name,
+		Path:                fileref.Path,
+		Hash:                fileref.Hash,
+		NumBlocks:           fileref.NumBlocks,
+		PathHash:            fileref.PathHash,
+		CustomMeta:          fileref.CustomMeta,
+		ContentHash:         fileref.ContentHash,
+		Size:                fileref.Size,
+		MerkleRoot:          fileref.MerkleRoot,
+		ActualFileSize:      fileref.ActualFileSize,
+		ActualFileHash:      fileref.ActualFileHash,
+		MimeType:            fileref.MimeType,
+		ThumbnailSize:       fileref.ThumbnailSize,
+		ThumbnailHash:       fileref.ThumbnailHash,
+		ActualThumbnailSize: fileref.ActualThumbnailSize,
+		ActualThumbnailHash: fileref.ActualThumbnailHash,
+		EncryptedKey:        fileref.EncryptedKey,
+		Attributes:          fileref.Attributes,
+		OnCloud:             fileref.OnCloud,
+		CommitMetaTxns:      commitMetaTxnsGRPC,
+		CreatedAt:           fileref.CreatedAt.UnixNano(),
+		UpdatedAt:           fileref.UpdatedAt.UnixNano(),
+		Collaborators:       collaboratorsGRPC,
+	}, nil
 }
