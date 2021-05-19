0ea4fb0ca16a52ad52593ae4428b75880022c9dd
Uk <is2pidguy1992@gmail.com>
2021-05-19 21:51:25 +0545

    Fix permission

--- sql/14-add-marketplace-table.sql
+++ sql/14-add-marketplace-table.sql
@@ -4,5 +4,7 @@
 CREATE TABLE marketplace (
     private_key VARCHAR(512) NOT NULL,
     public_key VARCHAR(512) NOT NULL,
-    mnemonic VARCHAR(512) NOT NULL,
+    mnemonic VARCHAR(512) NOT NULL
 );
+
+GRANT ALL PRIVILEGES ON TABLE marketplace TO blobber_user;
