176e4cd7e1f611d1f5787fd025e787ac9d443af4
Uk <is2pidguy1992@gmail.com>
2021-05-24 17:30:03 +0545

    Remove unused import

--- code/go/0chain.net/blobbercore/handler/blobber_meta_handler.go
+++ code/go/0chain.net/blobbercore/handler/blobber_meta_handler.go
@@ -6,17 +6,6 @@ import (
 	"context"
 	"net/http"
 
-	// "0chain.net/blobbercore/config"
-	// "0chain.net/blobbercore/constants"
-	// "0chain.net/blobbercore/datastore"
-	// "0chain.net/blobbercore/stats"
-	// "0chain.net/core/common"
-
-	// . "0chain.net/core/logging"
-	// "go.uber.org/zap"
-
-	// "github.com/gorilla/mux"
-
 	"0chain.net/blobbercore/reference"
 )
 
