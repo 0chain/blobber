bdde0c2e5ceb37c2e1c367851ecb54f2715817d3
Alex Guo <chessnut@outlook.com>
2021-06-07 20:11:41 +0000

    convert MIRACL public key to herumi/bls public key

--- code/go/0chain.net/core/encryption/keys.go
+++ code/go/0chain.net/core/encryption/keys.go
@@ -8,6 +8,7 @@ import (
 	"0chain.net/core/config"
 
 	"github.com/0chain/gosdk/core/zcncrypto"
+	"github.com/herumi/bls-go-binary/bls"
 )
 
 /*ReadKeys - reads a publicKey and a privateKey from a Reader.
@@ -37,4 +38,27 @@ func Verify(publicKey string, signature string, hash string) (bool, error) {
 		return signScheme.Verify(signature, hash)
 	}
 	return false, common.NewError("invalid_signature_scheme", "Invalid signature scheme. Please check configuration")
+}
+
+// If input is normal herumi/bls public key, it returns it immmediately.
+//   So this is completely backward compatible with herumi/bls.
+// If input is MIRACL public key, convert it to herumi/bls public key.
+//
+// This is an example of the raw public key we expect from MIRACL
+var miraclExamplePK = `0418a02c6bd223ae0dfda1d2f9a3c81726ab436ce5e9d17c531ff0a385a13a0b491bdfed3a85690775ee35c61678957aaba7b1a1899438829f1dc94248d87ed36817f6dfafec19bfa87bf791a4d694f43fec227ae6f5a867490e30328cac05eaff039ac7dfc3364e851ebd2631ea6f1685609fc66d50223cc696cb59ff2fee47ac`
+//
+// This is an example of the same MIRACL public key serialized with ToString().
+// pk ([1bdfed3a85690775ee35c61678957aaba7b1a1899438829f1dc94248d87ed368,18a02c6bd223ae0dfda1d2f9a3c81726ab436ce5e9d17c531ff0a385a13a0b49],[039ac7dfc3364e851ebd2631ea6f1685609fc66d50223cc696cb59ff2fee47ac,17f6dfafec19bfa87bf791a4d694f43fec227ae6f5a867490e30328cac05eaff])
+func MiraclToHerumiPK(pk string) string {
+	if len(pk) != len(miraclExamplePK) {
+		// If input is normal herumi/bls public key, it returns it immmediately.
+		return pk
+	}
+	n1 := pk[2:66]
+	n2 := pk[66:(66+64)]
+	n3 := pk[(66+64):(66+64+64)]
+	n4 := pk[(66+64+64):(66+64+64+64)]
+	var p bls.PublicKey
+	p.SetHexString("1 " + n2 + " " + n1 + " " + n4 + " " + n3)
+	return p.SerializeToHexStr()
 }
