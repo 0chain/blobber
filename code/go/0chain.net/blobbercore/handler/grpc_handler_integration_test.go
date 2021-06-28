77f6db76bcfd769eb70ddcb1fe0c86d60d475752
nipunapathirana <nsspathirana@gmail.com>
2021-06-28 23:03:08 +0530

    rename object handler integrated tests implemented

--- code/go/0chain.net/blobbercore/handler/grpc_handler_integration_test.go
+++ code/go/0chain.net/blobbercore/handler/grpc_handler_integration_test.go
@@ -1071,6 +1071,86 @@ func TestBlobberGRPCService_IntegrationTest(t *testing.T) {
 			}
 		}
 	})
+
+	t.Run("TestRenameObject", func(t *testing.T) {
+		allocationTx := randString(32)
+
+		pubKey, _, signScheme := GeneratePubPrivateKey(t)
+		clientSignature, _ := signScheme.Sign(encryption.Hash(allocationTx))
+		pubKeyBytes, _ := hex.DecodeString(pubKey)
+		clientId := encryption.Hash(pubKeyBytes)
+
+		if err := tdController.ClearDatabase(); err != nil {
+			t.Fatal(err)
+		}
+		if err := tdController.AddRenameTestData(allocationTx, pubKey, clientId); err != nil {
+			t.Fatal(err)
+		}
+
+		testCases := []struct {
+			name            string
+			context         metadata.MD
+			input           *blobbergrpc.RenameObjectRequest
+			expectedMessage string
+			expectingError  bool
+		}{
+			{
+				name: "Success",
+				context: metadata.New(map[string]string{
+					common.ClientHeader:          clientId,
+					common.ClientSignatureHeader: clientSignature,
+					common.ClientKeyHeader:       pubKey,
+				}),
+				input: &blobbergrpc.RenameObjectRequest{
+					Allocation:   allocationTx,
+					Path:         "/some_file",
+					PathHash:     "exampleId:examplePath",
+					ConnectionId: "connection_id",
+					NewName:      "some_new_file",
+				},
+				expectedMessage: "some_new_file",
+				expectingError:  false,
+			},
+			{
+				name: "Fail",
+				context: metadata.New(map[string]string{
+					common.ClientHeader:          clientId,
+					common.ClientSignatureHeader: clientSignature,
+					common.ClientKeyHeader:       pubKey,
+				}),
+				input: &blobbergrpc.RenameObjectRequest{
+					Allocation:   "",
+					Path:         "",
+					PathHash:     "",
+					ConnectionId: "",
+					NewName:      "",
+				},
+				expectedMessage: "",
+				expectingError:  true,
+			},
+		}
+
+		for _, tc := range testCases {
+			ctx := context.Background()
+			ctx = metadata.NewOutgoingContext(ctx, tc.context)
+			response, err := blobberClient.RenameObject(ctx, tc.input)
+			if err != nil {
+				if !tc.expectingError {
+					t.Fatal(err)
+				}
+
+				continue
+			}
+
+			if tc.expectingError {
+				t.Fatal("expected error")
+			}
+
+			if response.GetFilename() != tc.expectedMessage {
+				t.Fatal("failed!")
+			}
+		}
+	})
 }
 
 func randString(n int) string {
