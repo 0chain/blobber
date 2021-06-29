ac7d9c871f3283447841be1d4cc3e8a895871102
Lz <imlangzi@qq.com>
2021-06-29 20:55:53 +0800

    fix(upload&download): 0chain/zboxcli#36 removed transaction on migrate sql script

--- sql/15-add-chunk_size-to-reference_objects.sql
+++ sql/15-add-chunk_size-to-reference_objects.sql
@@ -5,8 +5,6 @@
 -- pew-pew
 \connect blobber_meta;
 
--- in a transaction
-BEGIN;
-    ALTER TABLE reference_objects
-        ADD COLUMN chunk_size INT NOT NULL DEFAULT 65536;
-COMMIT;
+
+ALTER TABLE reference_objects
+    ADD COLUMN chunk_size INT NOT NULL DEFAULT 65536;
