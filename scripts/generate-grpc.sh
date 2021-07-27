9bac9bae864e1f6dd942f38daed3d5b609c65e6c
kushthedude <kushthedude@gmail.com>
2021-07-27 19:42:10 +0530

    chore: clean the script folder with redundant one liner
    Signed-off-by: kushthedude <kushthedude@gmail.com>

--- scripts/generate-grpc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-protoc -I ./code/go/0chain.net/blobbercore/blobbergrpc/proto --go-grpc_out=. --go_out=. --grpc-gateway_out=allow_delete_body=true:. --openapiv2_opt allow_delete_body=true --openapiv2_out=./code/go/0chain.net/blobbercore/openapi ./code/go/0chain.net/blobbercore/blobbergrpc/proto/blobber.proto
