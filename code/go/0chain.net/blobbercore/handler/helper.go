937c1964c55fa45ff0c12f91ed8b651be012577b
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2021-08-01 10:44:26 +0545

    Fix: Remove unrequired function

--- code/go/0chain.net/blobbercore/handler/helper.go
+++ code/go/0chain.net/blobbercore/handler/helper.go
@@ -1,25 +1,10 @@
 package handler
 
 import (
-	"context"
 	"time"
 
-	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/blobbergrpc"
 	"github.com/0chain/blobber/code/go/0chain.net/core/common"
-	"github.com/gorilla/mux"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"google.golang.org/grpc"
 )
-
-func registerGRPCServices(r *mux.Router, server *grpc.Server) {
-	blobberService := newGRPCBlobberService()
-	grpcGatewayHandler := runtime.NewServeMux()
-
-	blobbergrpc.RegisterBlobberServer(server, blobberService)
-	_ = blobbergrpc.RegisterBlobberHandlerServer(context.Background(), grpcGatewayHandler, blobberService)
-	r.PathPrefix("/").Handler(grpcGatewayHandler)
-
-}
 
 func checkValidDate(s string) error {
 	if s != "" {
