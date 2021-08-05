b360cec6c3860fef778269bc14d9e5274fe0f5fe
Lz <imlangzi@qq.com>
2021-08-05 23:44:25 +0800

    fix(lock):#294 fixed memory leak issue

--- code/go/0chain.net/core/lock/lock.go
+++ code/go/0chain.net/core/lock/lock.go
@@ -1,17 +1,70 @@
 package lock
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
-var lockPool = make(map[string]*sync.Mutex, 0) //nolint:gosimple // need more time to verify: declaring capacity is probably necessary?
-var lockMutex = &sync.Mutex{}
+var (
+	lockPool   = make(map[string]*Mutex)
+	unlockPool = make(map[string]time.Time)
+	lockMutex  sync.Mutex
+)
 
-func GetMutex(tablename string, key string) *sync.Mutex {
+// Mutex a mutual exclusion lock.
+type Mutex struct {
+	// key lock key in pool
+	key string
+	sync.Mutex
+}
+
+// Lock implements Locker.Lock
+func (m *Mutex) Lock() {
+	m.Mutex.Lock()
+}
+
+// Unlock implements Locker.Unlock, and mark mutex as unlock object
+func (m *Mutex) Unlock() {
+	lockMutex.Lock()
+	defer lockMutex.Unlock()
+
+	m.Mutex.Unlock()
+	//mark it as unlock object, it will be deleted in clean worker
+	unlockPool[m.key] = time.Now()
+}
+
+// GetMutex get mutex by table and key
+func GetMutex(tablename string, key string) *Mutex {
 	lockKey := tablename + ":" + key
 	lockMutex.Lock()
 	defer lockMutex.Unlock()
 	if eLock, ok := lockPool[lockKey]; ok {
+		// do NOT remove it from pool
+		delete(unlockPool, lockKey)
 		return eLock
 	}
-	lockPool[lockKey] = &sync.Mutex{}
-	return lockPool[lockKey]
+
+	m := &Mutex{key: lockKey}
+	lockPool[lockKey] = m
+
+	return m
+}
+
+func init() {
+	go startLockPoolCleaner()
+}
+
+func startLockPoolCleaner() {
+	for {
+		time.Sleep(1 * time.Hour)
+
+		lockMutex.Lock()
+
+		for key := range unlockPool {
+			delete(lockPool, key)
+		}
+
+		unlockPool = make(map[string]time.Time)
+		lockMutex.Unlock()
+	}
 }
