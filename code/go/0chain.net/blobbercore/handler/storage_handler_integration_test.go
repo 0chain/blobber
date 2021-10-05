519f65b858a68856b59b399549c0300f4f682aef
Aman Jain <amanjain5221@gmail.com>
2021-10-05 22:55:37 +0530

    integration test

---
+++ code/go/0chain.net/blobbercore/handler/storage_handler_integration_test.go
@@ -0,0 +1,72 @@
+package handler
+
+import (
+	"context"
+	blobbergrpc "github.com/0chain/blobber/code/go/0chain.net/blobbercore/blobbergrpc/proto"
+	"github.com/0chain/blobber/code/go/0chain.net/core/common"
+	"github.com/0chain/blobber/code/go/0chain.net/core/encryption"
+	"google.golang.org/grpc/metadata"
+	"net/http"
+	"testing"
+)
+
+func TestBlobberGRPCService_MarketplaceShareInfo(t *testing.T) {
+	bClient, tdController := setupHandlerIntegrationTests(t)
+	allocationTx := randString(32)
+
+	pubKey, _, signScheme := GeneratePubPrivateKey(t)
+	clientSignature, _ := signScheme.Sign(encryption.Hash(allocationTx))
+
+	err := tdController.ClearDatabase()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	path := "/file.txt"
+	filePathHash := "f15383a1130bd2fae1e52a7a15c432269eeb7def555f1f8b9b9a28bd9611362c"
+	authTicket, err := GetAuthTicketForEncryptedFile(allocationTx, path, filePathHash, clientSignature, pubKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testCases := []struct {
+		name           string
+		context        metadata.MD
+		input          *blobbergrpc.MarketplaceShareInfoRequest
+		expectingError bool
+	}{
+		{
+			name: "Success",
+			context: metadata.New(map[string]string{
+				common.ClientHeader:          "exampleOwnerId",
+				common.ClientSignatureHeader: clientSignature,
+				common.ClientKeyHeader:       pubKey,
+			}),
+			input: &blobbergrpc.MarketplaceShareInfoRequest{
+				Allocation:          allocationTx,
+				EncryptionPublicKey: pubKey,
+				AuthTicket:          authTicket,
+				HttpMethod:          http.MethodPost,
+				Path:                "/file.txt",
+				RefereeClientId:     "",
+			},
+			expectingError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		ctx := context.Background()
+		ctx = metadata.NewOutgoingContext(ctx, tc.context)
+		_, err := bClient.MarketplaceShareInfo(ctx, tc.input)
+		if err != nil {
+			if !tc.expectingError {
+				t.Fatal(err)
+			}
+			continue
+		}
+
+		if tc.expectingError {
+			t.Fatal("expected error")
+		}
+	}
+}
