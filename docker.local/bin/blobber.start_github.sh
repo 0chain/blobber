bbd138bd9fea2e4db4250a405ca09972cb88a083
Lz <imlangzi@qq.com>
2021-11-10 22:53:02 +0800

    fix(devops): run docker-compose as deamon

--- docker.local/bin/blobber.start_github.sh
+++ docker.local/bin/blobber.start_github.sh
@@ -6,4 +6,4 @@ BLOBBER_DIR=`basename $PWD`
 BLOBBER_ID=`echo my directory $BLOBBER_DIR | sed -e 's/.*\(.\)$/\1/'`
 
 
-BLOBBER=$BLOBBER_ID docker-compose -p blobber$BLOBBER_ID -f ../b0docker-compose-github.yml up
+BLOBBER=$BLOBBER_ID docker-compose -p blobber$BLOBBER_ID -f ../b0docker-compose-github.yml up -d
