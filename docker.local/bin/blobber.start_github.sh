6b3ee94f17c49f5138b189544c3d26366ade1f86
Lz <imlangzi@qq.com>
2021-11-10 22:31:37 +0800

    fix(devops): added new shell script and docker-compose for github action

---
+++ docker.local/bin/blobber.start_github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+PWD=`pwd`
+BLOBBER_DIR=`basename $PWD`
+BLOBBER_ID=`echo my directory $BLOBBER_DIR | sed -e 's/.*\(.\)$/\1/'`
+
+
+BLOBBER=$BLOBBER_ID docker-compose -p blobber$BLOBBER_ID -f ../b0docker-compose-github.yml up
