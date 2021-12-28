d5df0de3f35bc0028b0b7c13ad154aa9bc0fcdd5
Pradip Parmar <pradip@alfa-creator.com>
2021-12-28 13:49:26 +0530

    delete refactor and issue fix.
    Signed-off-by: Pradip Parmar <pradip@alfa-creator.com>

--- code/go/0chain.net/blobbercore/reference/object.go
+++ code/go/0chain.net/blobbercore/reference/object.go
@@ -2,6 +2,7 @@ package reference
 
 import (
 	"context"
+	"path/filepath"
 	"strings"
 
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/datastore"
@@ -15,11 +16,8 @@ func LoadObjectTree(ctx context.Context, allocationID, path string) (*Ref, error
 	db := datastore.GetStore().
 		GetTransaction(ctx)
 
-	if strings.HasSuffix(path, "/") {
-		db = db.Where("allocation_id = ? and deleted_at IS NULL and path LIKE ? ", allocationID, (path + "%"))
-	} else {
-		db = db.Where("allocation_id = ? and deleted_at IS NULL and path LIKE ? ", allocationID, (path + "/%"))
-	}
+	path = filepath.Join("/", path)
+	db = db.Where("allocation_id = ? and deleted_at IS NULL and path LIKE ? ", allocationID, (path + "%"))
 
 	db = db.Order("level desc, lookup_hash")
 
@@ -30,16 +28,11 @@ func LoadObjectTree(ctx context.Context, allocationID, path string) (*Ref, error
 	err := db.FindInBatches(&objects, 100, func(tx *gorm.DB, batch int) error {
 		// batch processing found records
 		for _, object := range objects {
-			parent, ok := obejctTreeNodes[object.ParentPath]
-			if !ok {
-				parent = make([]*Ref, 0)
-			}
 
-			obejctTreeNodes[object.ParentPath] = append(parent, object)
+			obejctTreeNodes[object.ParentPath] = append(obejctTreeNodes[object.ParentPath], object)
 
-			child, ok := obejctTreeNodes[object.Path]
-			if ok {
-				object.Children = child
+			for _, child := range obejctTreeNodes[object.Path] {
+				object.AddChild(child)
 			}
 
 		}
@@ -74,20 +67,21 @@ func LoadObjectTree(ctx context.Context, allocationID, path string) (*Ref, error
 // DeleteObject delete object from tree, and return tree root and deleted content hash list
 func DeleteObject(ctx context.Context, allocationID string, path string) (*Ref, map[string]bool, error) {
 
+	rootRef, err := LoadObjectTree(ctx, allocationID, "/")
+	if err != nil {
+		return nil, nil, err
+	}
+
 	db := datastore.GetStore().
 		GetTransaction(ctx)
 
 	var deletedObjects []*Ref
 	txDelete := db.Clauses(clause.Returning{Columns: []clause.Column{{Name: "content_hash"}, {Name: "type"}}})
 
-	if strings.HasSuffix(path, "/") {
-		txDelete = txDelete.Where("allocation_id = ? and deleted_at IS NULL and (path LIKE ? or path = ?) and path != ? ", allocationID, (path + "%"), path, "/")
-	} else {
-		txDelete = txDelete.Where("allocation_id = ? and deleted_at IS NULL and (path LIKE ? or path = ?) and path != ? ", allocationID, (path + "/%"), path, "/")
-	}
+	path = filepath.Join("/", path)
+	txDelete = txDelete.Where("allocation_id = ? and deleted_at IS NULL and (path LIKE ? or path = ?) and path != ? ", allocationID, (path + "%"), path, "/")
 
-	err := txDelete.Delete(&deletedObjects).Error
-
+	err = txDelete.Delete(&deletedObjects).Error
 	if err != nil {
 		return nil, nil, common.NewError("bad_db_operation", err.Error())
 	}
@@ -99,11 +93,38 @@ func DeleteObject(ctx context.Context, allocationID string, path string) (*Ref, 
 		}
 	}
 
-	rootRef, err := LoadObjectTree(ctx, allocationID, "/")
-
-	if err != nil {
-		return nil, nil, err
+	// remove deleted object from tree
+	if path == "/" {
+		rootRef.Children = nil
+		return rootRef, deletedFiles, nil
 	}
 
-	return rootRef, deletedFiles, nil
+	path = strings.TrimSuffix(path, "/")
+	tSubDirs := GetSubDirsFromPath(path)
+
+	dirRef := rootRef
+	treelevel := 0
+	for treelevel < len(tSubDirs)-1 {
+		found := false
+		for _, child := range dirRef.Children {
+			if child.Name == tSubDirs[treelevel] && child.Type == DIRECTORY {
+				dirRef = child
+				found = true
+				break
+			}
+		}
+		if !found {
+			return nil, nil, common.NewError("invalid_reference_path", "Invalid reference path from the blobber")
+		}
+		treelevel++
+	}
+
+	for i, child := range dirRef.Children {
+		if child.Path == path {
+			dirRef.RemoveChild(i)
+			return rootRef, deletedFiles, nil
+		}
+	}
+	return nil, nil, common.NewError("invalid_reference_path", "Invalid reference path from the blobber")
+
 }
