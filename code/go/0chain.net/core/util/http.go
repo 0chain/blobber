6b7e34ddb056ea3dc0052b79970e06501fc1033e
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-01-17 16:25:23 +0545

    Fix wastedassign linter issue and refactor code

--- code/go/0chain.net/core/util/http.go
+++ code/go/0chain.net/core/util/http.go
@@ -19,16 +19,20 @@ import (
 const MAX_RETRIES = 5
 const SLEEP_BETWEEN_RETRIES = 5
 
-func NewHTTPRequest(method string, url string, data []byte) (*http.Request, context.Context, context.CancelFunc, error) {
+func NewHTTPRequest(method string, url string, data []byte) (req *http.Request, ctx context.Context, cncl context.CancelFunc, err error) {
 	requestHash := encryption.Hash(data)
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
+	req, err = http.NewRequest(method, url, bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	req.Header.Set("Access-Control-Allow-Origin", "*")
 	req.Header.Set("X-App-Client-ID", node.Self.ID)
 	req.Header.Set("X-App-Client-Key", node.Self.PublicKey)
 	req.Header.Set("X-App-Request-Hash", requestHash)
-	ctx, cncl := context.WithTimeout(context.Background(), time.Second*10)
-	return req, ctx, cncl, err
+	ctx, cncl = context.WithTimeout(context.Background(), time.Second*10)
+	return
 }
 
 func SendMultiPostRequest(urls []string, data []byte) {
@@ -41,37 +45,43 @@ func SendMultiPostRequest(urls []string, data []byte) {
 	wg.Wait()
 }
 
-func SendPostRequest(url string, data []byte, wg *sync.WaitGroup) (
-	body []byte, err error) {
-
+func SendPostRequest(url string, data []byte, wg *sync.WaitGroup) (body []byte, err error) {
 	if wg != nil {
 		defer wg.Done()
 	}
-	var resp *http.Response
-	for i := 0; i < MAX_RETRIES; i++ {
-		var (
-			req  *http.Request
-			ctx  context.Context
-			cncl context.CancelFunc
-		)
 
+	var (
+		resp *http.Response
+		req  *http.Request
+		ctx  context.Context
+		cncl context.CancelFunc
+	)
+
+	for i := 0; i < MAX_RETRIES; i++ {
 		req, ctx, cncl, err = NewHTTPRequest(http.MethodPost, url, data)
-		defer cncl()
+		if err != nil {
+			return nil, err
+		}
 
 		resp, err = http.DefaultClient.Do(req.WithContext(ctx))
 		if err == nil {
 			if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 				break
 			}
-			body, _ = ioutil.ReadAll(resp.Body)
-			if resp.Body != nil {
-				resp.Body.Close()
+			body, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				err = common.NewError("read_error", err.Error())
+			} else {
+				err = common.NewError("http_error", "Error from HTTP call. "+string(body))
 			}
-			err = common.NewError("http_error", "Error from HTTP call. "+string(body))
+			resp.Body.Close()
 		}
 		//TODO: Handle ctx cncl
 		time.Sleep(SLEEP_BETWEEN_RETRIES * time.Second)
 	}
+
+	defer cncl()
+
 	if resp == nil || err != nil {
 		Logger.Error("Failed after multiple retries", zap.Any("url", url), zap.Int("retried", MAX_RETRIES), zap.Error(err))
 		return nil, err
@@ -79,9 +89,8 @@ func SendPostRequest(url string, data []byte, wg *sync.WaitGroup) (
 	if resp.Body == nil {
 		return nil, common.NewError("empty_body", "empty body returned")
 	}
-	if resp.Body != nil {
-		defer resp.Body.Close()
-	}
+
+	defer resp.Body.Close()
 
 	body, err = ioutil.ReadAll(resp.Body)
 	return body, err
