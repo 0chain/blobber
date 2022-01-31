d7536ed862624c78d265aedc050420c28da75dca
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-01-31 15:31:27 +0545

    Fix issue raised by gosec linter

--- code/go/0chain.net/blobbercore/convert/response_creator.go
+++ code/go/0chain.net/blobbercore/convert/response_creator.go
@@ -29,7 +29,8 @@ func GetFileMetaDataResponseCreator(httpResp interface{}) *blobbergrpc.GetFileMe
 
 	var resp blobbergrpc.GetFileMetaDataResponse
 	collaborators, _ := r["collaborators"].([]reference.Collaborator)
-	for _, c := range collaborators {
+	for i := 0; i < len(collaborators); i++ {
+		c := collaborators[i]
 		resp.Collaborators = append(resp.Collaborators, CollaboratorToGRPCCollaborator(&c))
 	}
 
@@ -171,7 +172,8 @@ func CollaboratorResponseCreator(r interface{}) *blobbergrpc.CollaboratorRespons
 	}
 
 	collabs, _ := r.([]reference.Collaborator)
-	for _, c := range collabs {
+	for i := 0; i < len(collabs); i++ {
+		c := collabs[i]
 		resp.Collaborators = append(resp.Collaborators, CollaboratorToGRPCCollaborator(&c))
 	}
 
