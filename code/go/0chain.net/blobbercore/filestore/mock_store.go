371ebeeb345a8646ad3e954249394112d9e30947
Pradip Parmar <pradip@alfa-creator.com>
2022-02-22 09:54:25 +0530

    unit test for update file with store check.
    Signed-off-by: Pradip Parmar <pradip@alfa-creator.com>

--- code/go/0chain.net/blobbercore/filestore/mock_store.go
+++ code/go/0chain.net/blobbercore/filestore/mock_store.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"io"
 	"mime/multipart"
 
@@ -12,15 +13,17 @@ import (
 )
 
 type MockStore struct {
+	d map[string]map[string]bool
 }
 
 var mockStore *MockStore
 
-func UseMock() {
+func UseMock(initData map[string]map[string]bool) {
 	if mockStore == nil {
-		mockStore = &MockStore{}
+		mockStore = &MockStore{d: initData}
 	}
 
+	mockStore.d = initData
 	fileStore = mockStore
 }
 
@@ -67,6 +70,7 @@ func (ms *MockStore) GetFileBlock(allocationID string, fileData *FileInputData, 
 }
 
 func (ms *MockStore) CommitWrite(allocationID string, fileData *FileInputData, connectionID string) (bool, error) {
+	ms.addFileInDataObj(allocationID, fileData.Hash)
 	return true, nil
 }
 
@@ -74,8 +78,13 @@ func (ms *MockStore) GetFileBlockForChallenge(allocationID string, fileData *Fil
 	return nil, nil, constants.ErrNotImplemented
 }
 func (ms *MockStore) DeleteFile(allocationID, contentHash string) error {
+	if ms.d == nil || ms.d[allocationID] == nil || !ms.d[allocationID][contentHash] {
+		return errors.New("file not available related to content")
+	}
+	delete(ms.d[allocationID], contentHash)
 	return nil
 }
+
 func (ms *MockStore) GetTotalDiskSizeUsed() (int64, error) {
 	return 0, constants.ErrNotImplemented
 }
@@ -96,4 +105,15 @@ func (ms *MockStore) DownloadFromCloud(fileHash, filePath string) error {
 }
 func (ms *MockStore) SetupAllocation(allocationID string, skipCreate bool) (*StoreAllocation, error) {
 	return nil, constants.ErrNotImplemented
+}
+
+func (ms *MockStore) addFileInDataObj(allocationID, contentHash string) {
+	if contentHash == "" {
+		return
+	}
+	if ms.d == nil {
+		ms.d = make(map[string]map[string]bool, 0)
+	}
+	dataObj := ms.d[allocationID]
+	dataObj[contentHash] = true
 }
