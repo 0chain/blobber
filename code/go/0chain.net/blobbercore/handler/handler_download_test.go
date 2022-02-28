24312dbcbb0dc273b3fe012c7087013cec198e7a
Ayoub BOUROUMINE <47432728+abouroumine@users.noreply.github.com>
2022-02-28 10:38:52 +0100

    issue/524
    Fixed Tests Conflicts.

--- code/go/0chain.net/blobbercore/handler/handler_download_test.go
+++ code/go/0chain.net/blobbercore/handler/handler_download_test.go
@@ -742,7 +742,7 @@ func TestHandlers_Download_Requiring_Signature(t *testing.T) {
 					WillReturnError(gorm.ErrRecordNotFound)
 
 				rootPathHash := fileref.GetReferenceLookup(alloc.Tx, "/")
-				mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "reference_objects" WHERE`)).
+				mock.ExpectQuery(regexp.QuoteMeta(`SELECT "id","path" FROM "reference_objects" WHERE`)).
 					WithArgs(alloc.ID, rootPathHash).
 					WillReturnRows(
 						sqlmock.NewRows([]string{"path", "type", "path_hash", "lookup_hash", "content_hash", "encrypted_key", "parent_path"}).
@@ -888,7 +888,7 @@ func TestHandlers_Download_Requiring_Signature(t *testing.T) {
 					WillReturnError(gorm.ErrRecordNotFound)
 
 				rootPathHash := fileref.GetReferenceLookup(alloc.Tx, "/folder1")
-				mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "reference_objects" WHERE`)).
+				mock.ExpectQuery(regexp.QuoteMeta(`SELECT "id","path" FROM "reference_objects" WHERE`)).
 					WithArgs(alloc.ID, rootPathHash).
 					WillReturnRows(
 						sqlmock.NewRows([]string{"path", "type", "path_hash", "lookup_hash", "content_hash", "encrypted_key", "parent_path"}).
@@ -1030,7 +1030,7 @@ func TestHandlers_Download_Requiring_Signature(t *testing.T) {
 					)
 
 				rootPathHash := fileref.GetReferenceLookup(alloc.Tx, "/folder1")
-				mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "reference_objects" WHERE`)).
+				mock.ExpectQuery(regexp.QuoteMeta(`SELECT "id","path" FROM "reference_objects" WHERE`)).
 					WithArgs(alloc.ID, rootPathHash).
 					WillReturnRows(
 						sqlmock.NewRows([]string{"path", "type", "path_hash", "lookup_hash", "content_hash", "encrypted_key", "parent_path"}).
