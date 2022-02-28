4fad4adf65fca48b4c663f89bdbb77aa12732c37
rizary <andika.riyan@gmail.com>
2022-02-28 10:08:04 +0700

    change error output

--- code/go/0chain.net/blobbercore/handler/object_operation_handler.go
+++ code/go/0chain.net/blobbercore/handler/object_operation_handler.go
@@ -292,7 +292,7 @@ func (fsh *StorageHandler) DownloadFile(ctx context.Context, r *http.Request) (r
 		// we use Truncate because we work with UTC in the first place.
 		availableTime := shareInfo.AvailableAt.Truncate(d).Unix()
 		if common.Timestamp(availableTime) > common.Now() {
-			return nil, errors.New("the file is not available until: " + shareInfo.AvailableAt.UTC().Format("2006-01-02T15:04:05"))
+			return nil, common.NewErrorf("download_file", "the file is not available until: %v", shareInfo.AvailableAt.UTC().Format("2006-01-02T15:04:05"))
 		}
 
 		readMarker.AuthTicket = datatypes.JSON(authTokenString)
