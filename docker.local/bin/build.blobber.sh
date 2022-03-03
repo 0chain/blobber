eff42895d9dfb24ae88a17b7b79ccd463d81838c
Lz <imlangzi@qq.com>
2022-03-03 17:16:01 +0800

    fix(devops): fixed docker buildx layer

--- docker.local/bin/build.blobber.sh
+++ docker.local/bin/build.blobber.sh
@@ -6,7 +6,7 @@ echo $GIT_COMMIT
 
 if [ -z "$DOCKER_BUILD" ]; then  
     if [ "x86_64" != "$(uname -m)" ]; then
-        docker buildx create --use
+        docker buildx use blobber_buildx || docker buildx create --name blobber_buildx --use
         DOCKER_BUILD="buildx build --platform linux/arm64"
     else
         DOCKER_BUILD="build"
