957fd8b7a2707454ae7415f47f56ece2d2add4b6
Pradip Parmar <pradip@alfa-creator.com>
2022-03-08 07:56:36 +0530

    columns added in table
    Signed-off-by: Pradip Parmar <pradip@alfa-creator.com>

--- code/go/0chain.net/blobbercore/datastore/postgres_releases.go
+++ code/go/0chain.net/blobbercore/datastore/postgres_releases.go
@@ -1,12 +1,17 @@
 package datastore
 
+import "time"
+
 // releases migration histories
 var releases = []Migration{
-	// {
-	// 	Version:   "0.1.0",
-	// 	CreatedAt: time.Date(2021, 10, 15, 0, 0, 0, 0, time.UTC),
-	// 	Scripts: []string{
-	// 		"CREATE INDEX idx_allocation_path ON reference_objects (allocation_id,path,deleted_at);",
-	// 	},
-	// },
+	{
+		Version:   "0.1.0",
+		CreatedAt: time.Date(2022, 0, 30, 0, 0, 0, 0, time.UTC),
+		Scripts: []string{
+			`ALTER TABLE write_markers 
+			ADD name varchar(255),
+			ADD lookup_hash varchar(255),
+			ADD content_hash varchar(255);`,
+		},
+	},
 }
