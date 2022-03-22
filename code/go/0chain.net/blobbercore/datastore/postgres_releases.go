de43f755dd62c85373cc55e4ee6edb1b9a1fe6a2
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-03-22 14:36:23 +0545

    Fix lint issue

--- code/go/0chain.net/blobbercore/datastore/postgres_releases.go
+++ code/go/0chain.net/blobbercore/datastore/postgres_releases.go
@@ -1,12 +1,12 @@
 package datastore
 
 // releases migration histories
-var releases = []Migration{
-	// {
-	// 	Version:   "0.1.0",
-	// 	CreatedAt: time.Date(2021, 10, 15, 0, 0, 0, 0, time.UTC),
-	// 	Scripts: []string{
-	// 		"CREATE INDEX idx_allocation_path ON reference_objects (allocation_id,path,deleted_at);",
-	// 	},
-	// },
-}
+// var releases = []Migration{
+// {
+// 	Version:   "0.1.0",
+// 	CreatedAt: time.Date(2021, 10, 15, 0, 0, 0, 0, time.UTC),
+// 	Scripts: []string{
+// 		"CREATE INDEX idx_allocation_path ON reference_objects (allocation_id,path,deleted_at);",
+// 	},
+// },
+// }
