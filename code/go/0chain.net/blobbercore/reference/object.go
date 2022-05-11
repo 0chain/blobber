08ad9cf5021e0e65cac6c015d4c02f3fc910b320
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-05-11 18:25:48 +0545

    Set hashToBeComputed field for affected directories

--- code/go/0chain.net/blobbercore/reference/object.go
+++ code/go/0chain.net/blobbercore/reference/object.go
@@ -5,9 +5,11 @@ import (
 	"path/filepath"
 
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/datastore"
+	"github.com/0chain/blobber/code/go/0chain.net/core/common"
 )
 
 func DeleteObject(ctx context.Context, allocationID, objPath string) (*Ref, error) {
+
 	db := datastore.GetStore().GetTransaction(ctx)
 	err := db.Delete(&Ref{}, "allocation_id=? AND path LIKE ? AND path != ?",
 		allocationID, objPath+"%", "/").Delete(&Ref{}).Error
@@ -16,10 +18,31 @@ func DeleteObject(ctx context.Context, allocationID, objPath string) (*Ref, erro
 		return nil, err
 	}
 
-	rootRef, err := GetReferencePath(ctx, allocationID, filepath.Dir(objPath))
+	parentPath := filepath.Dir(objPath)
+	rootRef, err := GetReferencePath(ctx, allocationID, parentPath)
 	if err != nil {
 		return nil, err
 	}
+
+	subDirs := GetSubDirsFromPath(parentPath)
+	dirRef := rootRef
+
+	for _, subDir := range subDirs {
+		var found bool
+		for _, ref := range dirRef.Children {
+			if ref.Name == subDir {
+				ref.HashToBeComputed = true
+				found = true
+				dirRef = ref
+				break
+			}
+		}
+
+		if !found {
+			return nil, common.NewError("invalid_reference_path", "Reference path has invalid references")
+		}
+	}
+
 	rootRef.HashToBeComputed = true
 	return rootRef, nil
 }
