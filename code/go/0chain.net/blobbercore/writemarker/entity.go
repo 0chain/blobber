66f86fdcdf8bb90946067dd095edaf2ab2186df1
Lz <imlangzi@qq.com>
2022-07-24 20:39:21 +0800

    fix(gorm): fixed bigserial issue on AutoMigration

--- code/go/0chain.net/blobbercore/writemarker/entity.go
+++ code/go/0chain.net/blobbercore/writemarker/entity.go
@@ -51,7 +51,7 @@ type WriteMarkerEntity struct {
 	CloseTxnID      string            `gorm:"column:close_txn_id;size:64"`
 	ConnectionID    string            `gorm:"column:connection_id;size:64"`
 	ClientPublicKey string            `gorm:"column:client_key;size:256"`
-	Sequence        int64             `gorm:"column:sequence;unique;type:bigserial;<-:false"` // <-:false skips value insert/update by gorm
+	Sequence        int64             `gorm:"column:sequence;unique;type:bigint;AUTO_INCREMENT;<-:false"` // <-:false skips value insert/update by gorm
 	datastore.ModelWithTS
 }
 
