4034c0ec41c713f0c33843057d5b16d6d2122256
boddumanohar <b.manu199@gmail.com>
2022-08-31 16:16:40 +0200

    Update quick_blobbers.sh

--- docker.local/quick_blobbers.sh
+++ docker.local/quick_blobbers.sh
@@ -12,7 +12,7 @@ export BLOCK_WORKER_URL=http://${NETWORK}.0chain.net/dns
 
 #TODO: Fix docker installation
 sudo apt update
-sudo apt install -y build-essential jq wget unzip curl
+sudo apt install -y build-essential jq wget unzip curl docker.io
 
 # download docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
