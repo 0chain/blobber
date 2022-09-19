a282fd1ce2ab82a270d6b13af3a6488d845cb026
boddumanohar <b.manu199@gmail.com>
2022-09-19 17:10:42 +0200

    Update quick_blobbers.sh

--- docker.local/quick_blobbers.sh
+++ docker.local/quick_blobbers.sh
@@ -352,7 +352,7 @@ services:
       - ${PROJECT_ROOT}/keys_config:/validator/keysconfig
     ports:
       - "5061:31401"
-    command: ./bin/validator --port 31401 --hostname ${CLUSTER}.${DOMAIN} --deployment_mode 0 --keys_file keysconfig/b0bnode01_keys.txt --log_dir /validator/log
+    command: ./bin/validator --port 31401 --hostname ${CLUSTER}.${DOMAIN} --deployment_mode 0 --keys_file keysconfig/b0bnode02_keys.txt --log_dir /validator/log
     networks:
       default:
       testnet0:
