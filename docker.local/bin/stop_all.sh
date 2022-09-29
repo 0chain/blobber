ea317fdbe0a59c58c59a205341585b06c5250e22
service-0chain <service@0chain.net>
2022-09-29 11:46:25 +0200

    added custom postgres conf

--- docker.local/bin/stop_all.sh
+++ docker.local/bin/stop_all.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-cd docker.local/blobber1
+cd docker.local/blobber01
 ../bin/blobber.stop_bls.sh
 cd -
-cd docker.local/blobber2
+cd docker.local/blobber02
 ../bin/blobber.stop_bls.sh
 cd -
-cd docker.local/blobber3
+cd docker.local/blobber03
 ../bin/blobber.stop_bls.sh
 cd -
-cd docker.local/blobber4
+cd docker.local/blobber04
+../bin/blobber.stop_bls.sh
+cd -
+cd docker.local/blobber05
+../bin/blobber.stop_bls.sh
+cd -
+cd docker.local/blobber06
 ../bin/blobber.stop_bls.sh
