b9a7bc734f5157d56738266fe5f0ed3b483309bf
Tyrone Villaluna <avanaur@gmail.com>
2022-10-08 17:33:51 +1100

    API for Challenge Timing Details (#833)
    * WIP Challenge Timings
    
    * Fix
    
    * Add closed_at column to identify non-open challenges
    
    * Update query to use closed_at instead
    
    * Fix!

---
+++ code/go/0chain.net/blobbercore/handler/challenge_timings.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"context"
+	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/challenge"
+	"github.com/0chain/blobber/code/go/0chain.net/core/common"
+	"net/http"
+	"strconv"
+)
+
+func GetChallengeTimings(ctx context.Context, r *http.Request) (interface{}, error) {
+	var (
+		fromString = r.URL.Query().Get("from")
+		from       common.Timestamp
+	)
+
+	if fromString != "" {
+		fromI, err := strconv.Atoi(fromString)
+		if err != nil {
+			return nil, common.NewError("invalid_parameters", "from parameter is not valid")
+		}
+		from = common.Timestamp(fromI)
+	}
+
+	limit, err := common.GetOffsetLimitOrderParam(r.URL.Query())
+	if err != nil {
+		return nil, err
+	}
+
+	return challenge.GetChallengeTimings(from, limit)
+}
