7edfe02078060c4e165a2189840b08f4437e598b
Manohar Reddy <b.manu199@gmail.com>
2022-10-27 11:40:59 +0000

    add build_blobber.sh script

--- build_blobber.sh
+++ build_blobber.sh
@@ -1,8 +1,8 @@
 export BLOBBER_REGISTRY=bmanu199/blobber
-SHORT_SHA=$(echo ${{ env.SHA }} | head -c 8)
+SHORT_SHA=$(git rev-parse --short HEAD)
 export DOCKER_IMAGE_BASE="${BLOBBER_REGISTRY}:base"
-export DOCKER_BUILD="buildx build --platform linux/amd64,linux/arm64 --push"
-export DOCKER_IMAGE_BLOBBER="-t ${BLOBBER_REGISTRY}:${TAG} -t ${BLOBBER_REGISTRY}:${SHORT_SHA}"
+# export DOCKER_BUILD="buildx build --platform linux/amd64,linux/arm64 --push"
+export DOCKER_IMAGE_BLOBBER="-t ${BLOBBER_REGISTRY}:${SHORT_SHA}"
 
 ./docker.local/bin/build.base.sh && ./docker.local/bin/build.blobber.sh
-docker push ${BLOBBER_REGISTRY}:${SHORT_SHA}"
+docker push ${BLOBBER_REGISTRY}:${SHORT_SHA}
