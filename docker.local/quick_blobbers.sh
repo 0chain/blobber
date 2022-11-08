dd3ce8c4dd0de3a9352a82dbd741280a7a447cfd
boddumanohar <b.manu199@gmail.com>
2022-11-08 11:53:31 +0100

    Update quick_blobbers.sh

--- docker.local/quick_blobbers.sh
+++ docker.local/quick_blobbers.sh
@@ -18,7 +18,7 @@ export SERVICE_CHARGE="0.30"
 export GF_ADMIN_USER=0chaingfadminuser
 export GF_ADMIN_PASSWORD=0chaingfadminpassword
 export PROJECT_ROOT=/var/0chain/blobber
-export BLOCK_WORKER_URL=http://${NETWORK}.{DOMAIN}/dns
+export BLOCK_WORKER_URL=http://${NETWORK}.${DOMAIN}/dns
 
 
 ## cleanup server before starting the deployment
