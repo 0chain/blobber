079bd6e3c24357a449d5c9ab793be2cda1b0781e
Manohar Reddy <b.manu199@gmail.com>
2022-11-13 09:57:29 +0100

    init swagger

--- code/go/0chain.net/blobbercore/handler/handler.go
+++ code/go/0chain.net/blobbercore/handler/handler.go
@@ -1,3 +1,20 @@
+//	0chain Blobber Api:
+//	 version: 0.0.1
+//	 title: 0chain Blobber Api
+//	Schemes: http, https
+//	Host: localhost:7171
+//	BasePath: /
+//	Produces:
+//	  - application/json
+//
+// securityDefinitions:
+//
+//	apiKey:
+//	  type: apiKey
+//	  in: header
+//	  name: authorization
+//
+// swagger:meta
 package handler
 
 import (
