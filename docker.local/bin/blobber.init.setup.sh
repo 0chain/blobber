ebbfb2681f1c93fb9a404eed6874ac3627451851
shahnawaz-creator <msac8225@gmail.com>
2022-12-21 16:17:01 +0100

    fixed docker-deployment

--- docker.local/bin/blobber.init.setup.sh
+++ docker.local/bin/blobber.init.setup.sh
@@ -4,5 +4,8 @@ for i in $(seq 1 6)
 do
   mkdir -p docker.local/blobber$i/files
   mkdir -p docker.local/blobber$i/data/postgresql
-  mkdir -p docker.local/blobber$i/log	
+  mkdir -p docker.local/blobber$i/log
+  mkdir -p docker.local/validator$i/files
+  mkdir -p docker.local/validator$i/data/postgresql
+  mkdir -p docker.local/validator$i/log
 done
