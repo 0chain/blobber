2e619478dc01424a5bd2ef16345003f57fc4c547
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2022-12-27 18:34:26 +0530

    fix -p into script

--- docker.local/bin/blobber.init.setup.sh
+++ docker.local/bin/blobber.init.setup.sh
@@ -3,8 +3,8 @@
 for i in $(seq 1 6)
 do
   mkdir -p docker.local/blobber$i
-  mkdir /mnt/ssd/blobber$i
-  mkdir /mnt/hdd/blobber$i
+  mkdir -p /mnt/ssd/blobber$i
+  mkdir -p /mnt/hdd/blobber$i
   mkdir -p docker.local/validator$i
-  mkdir /mnt/hdd/validator$i
+  mkdir -p /mnt/hdd/validator$i
 done
