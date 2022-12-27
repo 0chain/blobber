eaeba2bc4a24ca565b6c0679b840142e20884cf8
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2022-12-27 18:08:42 +0530

    updated deployment wrt to hdd & ssd (#897)

--- docker.local/bin/clean.sh
+++ docker.local/bin/clean.sh
@@ -2,7 +2,8 @@
   
 for i in $(seq 1 6)
 do
-  rm -rf docker.local/blobber$i/log/*
-  rm -rf docker.local/blobber$i/data/postgresql/*
-  rm -rf docker.local/blobber$i/files/*
+  rm -rf /mnt/hdd/blobber$i
+  rm -rf /mnt/ssd/blobber$i
+  rm -rf /mnt/hdd/validator$i
+  rm -rf docker.local/blobber$i
 done
