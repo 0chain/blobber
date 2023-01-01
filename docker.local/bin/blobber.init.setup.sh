a8c35084401cf7e822ec4152968857c8852f8d12
shahnawaz-creator <msac8225@gmail.com>
2023-01-01 17:18:05 +0100

    fixed folder creation issue for mounting.

--- docker.local/bin/blobber.init.setup.sh
+++ docker.local/bin/blobber.init.setup.sh
@@ -2,9 +2,9 @@
 
 for i in $(seq 1 6)
 do
-  mkdir -p docker.local/blobber$i
-  mkdir -p /mnt/ssd/blobber$i
-  mkdir -p /mnt/hdd/blobber$i
-  mkdir -p docker.local/validator$i
-  mkdir -p /mnt/hdd/validator$i
+  sudo mkdir -p docker.local/blobber$i
+  sudo mkdir -p /mnt/ssd/blobber$i
+  sudo mkdir -p /mnt/hdd/blobber$i
+  sudo mkdir -p docker.local/validator$i
+  sudo mkdir -p /mnt/hdd/validator$i
 done
