164d00f8e7e4d8c29821b1aca2173f8309365d0b
boddumanohar <b.manu199@gmail.com>
2023-01-12 16:25:00 +0100

    Update migration.sh

--- docker.local/migration.sh
+++ docker.local/migration.sh
@@ -3,20 +3,26 @@
 MIGRATION_ROOT=$HOME/.s3migration
 SCRIPT=0chainmigrationscript
 
+sudo apt update
+sudo apt install -y unzip curl containerd docker.io
+
+echo "download docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+sudo curl -L "https://github.com/0chain/s3-migration/raw/staging/.bin/s3migration" -o /usr/local/bin/s3mgrtmigrate
+
 mkdir -p ${MIGRATION_ROOT}
 
 cat <<EOF >${PROJECT_ROOT}/config/docker-compose.yml
 version: '3.8'
 services:
   db:
-    image: postgres:13-alpine
+    image: bmanu199/s3mgrt:latest
     restart: always
-    command: -c "log_statement=all"
-    environment:
-      - POSTGRES_USER=postgres
-      - POSTGRES_PASSWORD=postgres
     ports:
-      - '5432:5432'
+      - '9012:9012'
     volumes:
       - ${MIGRATION_ROOT}:/root/.zcn
 EOF
