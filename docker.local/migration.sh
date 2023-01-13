05ae2da022b92d4a9dc0a885f6e935c5cbbb2915
boddumanohar <b.manu199@gmail.com>
2023-01-13 19:56:24 +0100

    Update migration.sh

--- docker.local/migration.sh
+++ docker.local/migration.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 MIGRATION_ROOT=$HOME/.s3migration
-SCRIPT=0chainmigrationscript
-
+ACCESS_KEY=0chainaccesskey
+SECRET_KEY=0chainsecretkey
+ALLOCATION=0chainallocation
+BUCKET=0chainbucket
+# ACCESS_KEY=AKIA4MPQDEZ4OACO45FD
+# SECRET_KEY=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
+# ALLOCATION=1467bdcedb20e62928eb18960c501c1c50ff35c927b86b34a8f0b00eb29a6d06
+# BUCKET=cloud-mig
+CONCURRENCY=1
+DELETE_SOURCE=false
+ENCRYPT=false
+REGION=us-east-2
+# BLOCK_WORKER_URL=0chainblockworkerurl
+BLOCK_WORKER_URL=https://demo.0chain.net/dns
 sudo apt update
 sudo apt install -y unzip curl containerd docker.io
 
@@ -11,20 +23,40 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-sudo curl -L "https://github.com/0chain/s3-migration/raw/staging/.bin/s3migration" -o /usr/local/bin/s3mgrtmigrate
+# sudo curl -L "https://github.com/0chain/s3-migration/raw/staging/.bin/s3migration" -o /usr/local/bin/s3mgrtmigrate
+# chmod +x /usr/local/bin/s3mgrtmigrate
 
 mkdir -p ${MIGRATION_ROOT}
 
-cat <<EOF >${PROJECT_ROOT}/config/docker-compose.yml
+cat <<EOF >${MIGRATION_ROOT}/docker-compose.yml
 version: '3.8'
 services:
   db:
     image: bmanu199/s3mgrt:latest
     restart: always
     ports:
-      - '9012:9012'
+      - '9012:8080'
     volumes:
-      - ${MIGRATION_ROOT}:/root/.zcn
+      - ${MIGRATION_ROOT}:/migrate
 EOF
 
-${SCRIPT}
+cat <<EOF >${MIGRATION_ROOT}/config.yaml
+block_worker: ${BLOCK_WORKER_URL}
+signature_scheme: bls0chain
+min_submit: 50
+min_confirmation: 50
+confirmation_chain_length: 3
+max_txn_query: 5
+query_sleep_time: 5
+# # OPTIONAL - Uncomment to use/ Add more if you want
+# preferred_blobbers:
+#   - http://one.devnet-0chain.net:31051
+#   - http://one.devnet-0chain.net:31052
+EOF
+
+/usr/local/bin/docker-compose -f ${MIGRATION_ROOT}/docker-compose.yml up -d
+
+#  --concurrency ${CONCURRENCY} --delete-source ${DELETE_SOURCE} --encrypt ${ENCRYPT} --resume true   --skip 1
+
+cd ${MIGRATION_ROOT}
+/usr/local/bin/s3mgrtmigrate migrate --access-key ${ACCESS_KEY} --secret-key ${SECRET_KEY} --allocation ${ALLOCATION} --bucket ${BUCKET} --region ${REGION}
