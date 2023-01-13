ab4bb70ba56f50eb8b363fc2eca7734ab7227aa9
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2023-01-13 09:19:32 +0530

    fixed folder creation issue for mounting. (#902)
    Co-authored-by: Lz <imlangzi@qq.com>

--- docker.local/bin/clean.sh
+++ docker.local/bin/clean.sh
@@ -2,8 +2,9 @@
   
 for i in $(seq 1 6)
 do
-  rm -rf /mnt/hdd/blobber$i
-  rm -rf /mnt/ssd/blobber$i
-  rm -rf /mnt/hdd/validator$i
-  rm -rf docker.local/blobber$i
+  sudo rm -rf /mnt/hdd/blobber$i
+  sudo rm -rf /mnt/ssd/blobber$i
+  sudo rm -rf /mnt/hdd/validator$i
+  sudo rm -rf docker.local/blobber$i
+  sudo rm -rf docker.local/validator$i
 done
