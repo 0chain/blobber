54c61f8eb11240c101431c48955fa4d050e31b49
boddumanohar <b.manu199@gmail.com>
2023-01-15 22:16:25 +0100

    Update blimp.sh

--- docker.local/blimp.sh
+++ docker.local/blimp.sh
@@ -4,7 +4,8 @@ CONFIG_DIR=$HOME/.zcn
 MINIO_USERNAME=0chainminiousername
 MINIO_PASSWORD=0chainminiopassword
 ALLOCATION=0chainallocationid
-BLOCK_WORKER_URL=0chainblockworker
+# BLOCK_WORKER_URL=0chainblockworker
+BLOCK_WORKER_URL=https://helm.0chain.net/dns
 # todo: check with team
 MINIO_TOKEN=0chainminiotoken
 
