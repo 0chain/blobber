707fe4b9b56de032bf445a75e73a061ec520525b
service-0chain <service@0chain.net>
2023-01-26 23:09:12 +0100

    testing fix for buildx issue on arc runners.

--- docker.local/bin/build.base.sh
+++ docker.local/bin/build.base.sh
@@ -28,4 +28,4 @@ echo "2> download herumi"
 
 echo ""
 echo "3> docker build"
-DOCKER_BUILDKIT=1 docker buildx build --name blobber_buildx --driver docker-container --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host' --build-arg GIT_COMMIT=$GIT_COMMIT -f docker.local/base.Dockerfile . -t $DOCKER_IMAGE_BASE
+DOCKER_BUILDKIT=1 docker buildx build --driver docker-container --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host' --build-arg GIT_COMMIT=$GIT_COMMIT -f docker.local/base.Dockerfile . -t $DOCKER_IMAGE_BASE
