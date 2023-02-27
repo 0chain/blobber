82b4ccd695c38d0e9b418d26a13f4fac42dfb9fe
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2023-02-27 23:50:13 +0530

    ReloadFromChain added

--- code/go/0chain.net/blobber/worker.go
+++ code/go/0chain.net/blobber/worker.go
@@ -7,6 +7,7 @@ import (
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/allocation"
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/challenge"
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/config"
+	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/datastore"
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/handler"
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/readmarker"
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/writemarker"
@@ -39,6 +40,28 @@ func refreshPriceOnChain(ctx context.Context) {
 			if err != nil {
 				logging.Logger.Error("refresh price on chain ", zap.Error(err))
 			}
+		}
+
+	}
+}
+
+// startRefreshSettings sync settings from blockchain
+func startRefreshSettings(ctx context.Context) {
+	const REPEAT_DELAY = 60 * 3 // 3 minutes
+	var err error
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(REPEAT_DELAY * time.Second):
+			_, err = config.ReloadFromChain(common.GetRootContext(), datastore.GetStore().GetDB())
+			if err != nil {
+				logging.Logger.Warn("failed to refresh blobber settings from chain", zap.Error(err))
+				continue
+			}
+
+			logging.Logger.Info("success to refresh blobber settings from chain")
+
 		}
 
 	}
