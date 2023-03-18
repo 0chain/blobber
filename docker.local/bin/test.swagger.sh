61fca98d5da5a4113b0979f9c97f3833b3379277
shahnawaz-creator <msac8225@gmail.com>
2023-03-18 22:27:17 +0100

    fix swagger docker buildx

--- docker.local/bin/test.swagger.sh
+++ docker.local/bin/test.swagger.sh
@@ -3,14 +3,14 @@
 set -e
 
 echo "1> set DOCKER_IMAGE & DOCKER_BUILD"
-# if [ -z "$DOCKER_BUILD" ]; then
-#     if [ "x86_64" != "$(uname -m)" ]; then
-#         #docker buildx use blobber_buildx || docker buildx create --name blobber_buildx --use
-#         DOCKER_BUILD="buildx build --platform linux/arm64"
-#     else
-#         DOCKER_BUILD="build"
-#     fi
-# fi
+if [ -z "$DOCKER_BUILD" ]; then
+    if [ "x86_64" != "$(uname -m)" ]; then
+        #docker buildx use blobber_buildx || docker buildx create --name blobber_buildx --use
+        DOCKER_BUILD="buildx build --platform linux/arm64"
+    else
+        DOCKER_BUILD="build"
+    fi
+fi
 
 if [ -z "$DOCKER_IMAGE_BASE" ]; then
     DOCKER_IMAGE_BASE="blobber_base"
@@ -19,7 +19,7 @@ fi
 dockerfile="docker.local/Dockerfile.swagger"
 platform=""
 
-DOCKER_BUILDKIT=1 docker $DOCKER_BUILD --progress=plain --build-arg DOCKER_IMAGE_BASE=$DOCKER_IMAGE_BASE -f $dockerfile . -t swagger_test
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm64 --progress=plain --build-arg DOCKER_IMAGE_BASE=$DOCKER_IMAGE_BASE -f $dockerfile . -t swagger_test
 
 echo "swagger_test docker image is successfully build"
 echo "print docker conatiners."
@@ -27,7 +27,7 @@ docker ps -a
 echo "print docker images"
 docker images
 
-docker run $platform $INTERACTIVE -v $(pwd):/codecov  swagger_test bash -c "\
+docker run $platform $INTERACTIVE -v $(pwd):/codecov swagger_test uname -a bash -c "\
 cd /codecov/code/go/0chain.net/; \
 swagger generate spec -w . -m -o swagger.yaml; \
 swagger generate markdown -f swagger.yaml --output=swagger.md"
