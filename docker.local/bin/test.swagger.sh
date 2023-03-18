6a0cf18ad2cc5dc53541b3ed457fdc60e265e3bf
shahnawaz-creator <msac8225@gmail.com>
2023-03-18 21:15:30 +0100

    fix swagger docker buildx

--- docker.local/bin/test.swagger.sh
+++ docker.local/bin/test.swagger.sh
@@ -16,30 +16,12 @@ if [ -z "$DOCKER_IMAGE_BASE" ]; then
     DOCKER_IMAGE_BASE="blobber_base"
 fi
 
-# cmd="build --build-arg DOCKER_IMAGE_BASE=$DOCKER_IMAGE_BASE"
-
 dockerfile="docker.local/Dockerfile.swagger"
 platform="linux/arm64"
 
-# for arg in "$@"
-# do
-#     case $arg in
-#         -m1|--m1|m1)
-#         echo "The build will be performed for Apple M1 chip"
-#         cmd="buildx build --platform linux/amd64"
-#         dockerfile="docker.local/build.unit_test/Dockerfile.m1"
-#         platform="--platform=linux/amd64"
-#         shift
-#         ;;
-#     esac
-# done
-
 DOCKER_BUILDKIT=1 docker $DOCKER_BUILD --progress=plain --build-arg DOCKER_IMAGE_BASE=$DOCKER_IMAGE_BASE -f $dockerfile . -t swagger_test
-docker buildx ls
-docker ps -a
 
 docker run $platform $INTERACTIVE -v $(pwd):/codecov  swagger_test bash -c "\
 cd /codecov/code/go/0chain.net/; \
 swagger generate spec -w . -m -o swagger.yaml; \
-swagger generate markdown -f swagger.yaml --output=swagger.md
-"
+swagger generate markdown -f swagger.yaml --output=swagger.md"
