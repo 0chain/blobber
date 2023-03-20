0a2fac2317716a77db3e9933db204c65bb61adc0
boddumanohar <b.manu199@gmail.com>
2023-03-20 10:37:33 +0100

    use latest docker compose

--- docker.local/quick_blobbers.sh
+++ docker.local/quick_blobbers.sh
@@ -31,7 +31,7 @@ sudo apt update
 sudo apt install -y unzip curl containerd docker.io ansible
 
 # download docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
