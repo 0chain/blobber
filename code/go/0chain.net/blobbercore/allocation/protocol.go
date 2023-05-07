5280a9391cd415bcb66bcdc124e110d333468d19
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-05-07 22:27:35 +0530

    Added logs to check blobberSize

--- code/go/0chain.net/blobbercore/allocation/protocol.go
+++ code/go/0chain.net/blobbercore/allocation/protocol.go
@@ -110,6 +110,11 @@ func VerifyAllocationTransaction(ctx context.Context, allocationTx string, reado
 			}
 			foundBlobber = true
 			a.AllocationRoot = ""
+
+			logging.Logger.Debug("jayash Blobber Details : ",
+				zap.Any("SASIZE", sa.Size),
+				zap.Any("SA", sa),
+				zap.Any("blobberDetails", sa.BlobberDetails))
 			a.BlobberSize = (sa.Size + int64(len(sa.BlobberDetails)-1)) /
 				int64(len(sa.BlobberDetails))
 			a.BlobberSizeUsed = 0
