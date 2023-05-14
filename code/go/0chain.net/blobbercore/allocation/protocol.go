3fcd2742247731aee92ed5d4016c9ce67319855c
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-05-14 12:27:35 +0530

    Revert "Fix allocation issue"
    This reverts commit 0ed12371

--- code/go/0chain.net/blobbercore/allocation/protocol.go
+++ code/go/0chain.net/blobbercore/allocation/protocol.go
@@ -74,11 +74,6 @@ func VerifyAllocationTransaction(ctx context.Context, allocationTx string, reado
 		return          // found in DB
 	}
 
-	updateAllocation(ctx, &Allocation{
-		ID: allocationTx,
-		Tx: allocationTx,
-	})
-
 	t, err := transaction.VerifyTransaction(allocationTx, chain.GetServerChain())
 	if err != nil {
 		return nil, common.NewError("invalid_allocation",
