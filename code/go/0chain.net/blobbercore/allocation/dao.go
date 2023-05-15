d0cceacf7c28bd88466184363a58e23d8cfbd730
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-05-15 22:48:39 +0530

    Fix allocations

--- code/go/0chain.net/blobbercore/allocation/dao.go
+++ code/go/0chain.net/blobbercore/allocation/dao.go
@@ -1,37 +1,31 @@
 package allocation
 
 import (
-	"context"
-
-	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/datastore"
-	"github.com/0chain/blobber/code/go/0chain.net/core/common"
-	"github.com/0chain/errors"
-	"github.com/0chain/gosdk/constants"
 	"gorm.io/gorm"
 )
 
-// GetOrCreate, get allocation if it exists in db. if not, try to sync it from blockchain, and insert it in db.
-func GetOrCreate(ctx context.Context, store datastore.Store, allocationTx string) (*Allocation, error) {
-	db := store.GetDB()
-
-	if len(allocationTx) == 0 {
-		return nil, errors.Throw(constants.ErrInvalidParameter, "tx")
-	}
-
-	alloc := &Allocation{}
-	result := db.Table(TableNameAllocation).Where(SQLWhereGetByTx, allocationTx).First(alloc)
-
-	if result.Error == nil {
-		return alloc, nil
-	}
-
-	if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return nil, errors.ThrowLog(result.Error.Error(), common.ErrBadDataStore)
-	}
-
-	return SyncAllocation(allocationTx)
-
-}
+//// GetOrCreate, get allocation if it exists in db. if not, try to sync it from blockchain, and insert it in db.
+//func GetOrCreate(ctx context.Context, store datastore.Store, allocationTx string) (*Allocation, error) {
+//	db := store.GetDB()
+//
+//	if len(allocationTx) == 0 {
+//		return nil, errors.Throw(constants.ErrInvalidParameter, "tx")
+//	}
+//
+//	alloc := &Allocation{}
+//	result := db.Table(TableNameAllocation).Where(SQLWhereGetByTx, allocationTx).First(alloc)
+//
+//	if result.Error == nil {
+//		return alloc, nil
+//	}
+//
+//	if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+//		return nil, errors.ThrowLog(result.Error.Error(), common.ErrBadDataStore)
+//	}
+//
+//	return SyncAllocation(allocationTx)
+//
+//}
 
 const (
 	SQLWhereGetByTx = "allocations.tx = ?"
--- code/go/0chain.net/blobbercore/allocation/zcn.go
+++ code/go/0chain.net/blobbercore/allocation/zcn.go
@@ -1,87 +1,75 @@
 package allocation
 
-import (
-	"encoding/json"
-
-	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/datastore"
-	"github.com/0chain/blobber/code/go/0chain.net/core/chain"
-	"github.com/0chain/blobber/code/go/0chain.net/core/common"
-	"github.com/0chain/blobber/code/go/0chain.net/core/node"
-	"github.com/0chain/blobber/code/go/0chain.net/core/transaction"
-	"github.com/0chain/errors"
-	"gorm.io/gorm"
-)
-
 // SyncAllocation try to pull allocation from blockchain, and insert it in db.
-func SyncAllocation(allocationTx string) (*Allocation, error) {
-	t, err := transaction.VerifyTransaction(allocationTx, chain.GetServerChain())
-	if err != nil {
-		return nil, errors.Throw(common.ErrBadRequest,
-			"Invalid Allocation id. Allocation not found in blockchain.")
-	}
-	var sa transaction.StorageAllocation
-	err = json.Unmarshal([]byte(t.TransactionOutput), &sa)
-	if err != nil {
-		return nil, errors.ThrowLog(err.Error(), common.ErrInternal, "Error decoding the allocation transaction output.")
-	}
-
-	alloc := &Allocation{}
-
-	belongToThisBlobber := false
-	for _, blobberConnection := range sa.BlobberDetails {
-		if blobberConnection.BlobberID == node.Self.ID {
-			belongToThisBlobber = true
-
-			alloc.AllocationRoot = ""
-			alloc.BlobberSize = (sa.Size + int64(len(sa.BlobberDetails)-1)) /
-				int64(len(sa.BlobberDetails))
-			alloc.BlobberSizeUsed = 0
-
-			break
-		}
-	}
-	if !belongToThisBlobber {
-		return nil, errors.Throw(common.ErrBadRequest,
-			"Blobber is not part of the open connection transaction")
-	}
-
-	// set/update fields
-	alloc.ID = sa.ID
-	alloc.Tx = sa.Tx
-	alloc.Expiration = sa.Expiration
-	alloc.OwnerID = sa.OwnerID
-	alloc.OwnerPublicKey = sa.OwnerPublicKey
-	alloc.RepairerID = t.ClientID // blobber node id
-	alloc.TotalSize = sa.Size
-	alloc.UsedSize = sa.UsedSize
-	alloc.Finalized = sa.Finalized
-	alloc.TimeUnit = sa.TimeUnit
-	alloc.FileOptions = sa.FileOptions
-
-	// related terms
-	terms := make([]*Terms, 0, len(sa.BlobberDetails))
-	for _, d := range sa.BlobberDetails {
-		terms = append(terms, &Terms{
-			BlobberID:    d.BlobberID,
-			AllocationID: alloc.ID,
-			ReadPrice:    d.Terms.ReadPrice,
-			WritePrice:   d.Terms.WritePrice,
-		})
-	}
-
-	err = datastore.GetStore().GetDB().Transaction(func(tx *gorm.DB) error {
-		if err := tx.Table(TableNameAllocation).Create(alloc).Error; err != nil {
-			return err
-		}
-
-		for _, term := range terms {
-			if err := tx.Table(TableNameTerms).Create(term).Error; err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
-
-	return alloc, err
-}
+//func SyncAllocation(allocationTx string) (*Allocation, error) {
+//	t, err := transaction.VerifyTransaction(allocationTx, chain.GetServerChain())
+//	if err != nil {
+//		return nil, errors.Throw(common.ErrBadRequest,
+//			"Invalid Allocation id. Allocation not found in blockchain.")
+//	}
+//	var sa transaction.StorageAllocation
+//	err = json.Unmarshal([]byte(t.TransactionOutput), &sa)
+//	if err != nil {
+//		return nil, errors.ThrowLog(err.Error(), common.ErrInternal, "Error decoding the allocation transaction output.")
+//	}
+//
+//	alloc := &Allocation{}
+//
+//	belongToThisBlobber := false
+//	for _, blobberConnection := range sa.BlobberDetails {
+//		if blobberConnection.BlobberID == node.Self.ID {
+//			belongToThisBlobber = true
+//
+//			alloc.AllocationRoot = ""
+//			alloc.BlobberSize = (sa.Size + int64(len(sa.BlobberDetails)-1)) /
+//				int64(len(sa.BlobberDetails))
+//			alloc.BlobberSizeUsed = 0
+//
+//			break
+//		}
+//	}
+//	if !belongToThisBlobber {
+//		return nil, errors.Throw(common.ErrBadRequest,
+//			"Blobber is not part of the open connection transaction")
+//	}
+//
+//	// set/update fields
+//	alloc.ID = sa.ID
+//	alloc.Tx = sa.Tx
+//	alloc.Expiration = sa.Expiration
+//	alloc.OwnerID = sa.OwnerID
+//	alloc.OwnerPublicKey = sa.OwnerPublicKey
+//	alloc.RepairerID = t.ClientID // blobber node id
+//	alloc.TotalSize = sa.Size
+//	alloc.UsedSize = sa.UsedSize
+//	alloc.Finalized = sa.Finalized
+//	alloc.TimeUnit = sa.TimeUnit
+//	alloc.FileOptions = sa.FileOptions
+//
+//	// related terms
+//	terms := make([]*Terms, 0, len(sa.BlobberDetails))
+//	for _, d := range sa.BlobberDetails {
+//		terms = append(terms, &Terms{
+//			BlobberID:    d.BlobberID,
+//			AllocationID: alloc.ID,
+//			ReadPrice:    d.Terms.ReadPrice,
+//			WritePrice:   d.Terms.WritePrice,
+//		})
+//	}
+//
+//	err = datastore.GetStore().GetDB().Transaction(func(tx *gorm.DB) error {
+//		if err := tx.Table(TableNameAllocation).Create(alloc).Error; err != nil {
+//			return err
+//		}
+//
+//		for _, term := range terms {
+//			if err := tx.Table(TableNameTerms).Create(term).Error; err != nil {
+//				return err
+//			}
+//		}
+//
+//		return nil
+//	})
+//
+//	return alloc, err
+//}
