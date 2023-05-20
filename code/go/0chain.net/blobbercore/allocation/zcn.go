14c86d34bc757edd8bb9f7d3a75fca676c71bdfa
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-05-20 19:08:14 +0530

    Fixed SyncAllocation

--- code/go/0chain.net/blobbercore/allocation/zcn.go
+++ code/go/0chain.net/blobbercore/allocation/zcn.go
@@ -1,22 +1,38 @@
 package allocation
 
 import (
+	"encoding/json"
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/datastore"
+	"github.com/0chain/blobber/code/go/0chain.net/core/chain"
 	"github.com/0chain/blobber/code/go/0chain.net/core/common"
 	"github.com/0chain/blobber/code/go/0chain.net/core/node"
+	"github.com/0chain/blobber/code/go/0chain.net/core/transaction"
 	"github.com/0chain/errors"
 	"gorm.io/gorm"
 )
 
 // SyncAllocation try to pull allocation using allocation ID from blockchain, and insert it in db.
 func SyncAllocation(allocationTx string) (*Allocation, error) {
-
-	sa, err := requestAllocation(allocationTx)
+	t, err := transaction.VerifyTransaction(allocationTx, chain.GetServerChain())
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to request allocation")
+		return nil, errors.Throw(common.ErrBadRequest,
+			"Invalid Allocation id. Allocation not found in blockchain.")
+	}
+	var sa transaction.StorageAllocation
+	err = json.Unmarshal([]byte(t.TransactionOutput), &sa)
+	if err != nil {
+		return nil, errors.ThrowLog(err.Error(), common.ErrInternal, "Error decoding the allocation transaction output.")
 	}
 
+	db := datastore.GetStore().GetDB()
 	alloc := &Allocation{}
+	result := db.Table(TableNameAllocation).Where("allocations.id = ?", sa.ID).First(alloc)
+
+	if result.Error == nil && alloc.ID == sa.ID {
+		return alloc, nil
+	}
+
+	alloc = &Allocation{}
 
 	belongToThisBlobber := false
 	for _, blobberConnection := range sa.BlobberDetails {
@@ -42,7 +58,7 @@ func SyncAllocation(allocationTx string) (*Allocation, error) {
 	alloc.Expiration = sa.Expiration
 	alloc.OwnerID = sa.OwnerID
 	alloc.OwnerPublicKey = sa.OwnerPublicKey
-	alloc.RepairerID = node.Self.ID // blobber node id
+	alloc.RepairerID = t.ClientID // blobber node id
 	alloc.TotalSize = sa.Size
 	alloc.UsedSize = sa.UsedSize
 	alloc.Finalized = sa.Finalized
