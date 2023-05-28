015b2ddd55c5345395b3cf7b2e7139838d9179db
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-05-28 14:59:20 +0530

    Fix header

--- code/go/0chain.net/blobbercore/handler/context.go
+++ code/go/0chain.net/blobbercore/handler/context.go
@@ -201,7 +201,7 @@ func WithVerify(r *http.Request) (*Context, error) {
 
 	ctx.ClientID = r.Header.Get(common.ClientHeader)
 	ctx.ClientKey = r.Header.Get(common.ClientKeyHeader)
-	ctx.AllocationId = r.Header.Get("allocation_id")
+	ctx.AllocationId = r.Header.Get("Allocation-Id")
 	ctx.Signature = r.Header.Get(common.ClientSignatureHeader)
 
 	logging.Logger.Info("jayash allocationID", zap.Any("allocationID", ctx.AllocationId))
