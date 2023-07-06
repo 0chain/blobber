62c6d47b73011adf329ce597e8387fd8f4a7e519
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-07-06 23:58:56 +0530

    Fix

--- code/go/0chain.net/blobbercore/handler/object_operation_handler.go
+++ code/go/0chain.net/blobbercore/handler/object_operation_handler.go
@@ -717,6 +717,10 @@ func (fsh *StorageHandler) CommitWrite(ctx context.Context, r *http.Request) (*b
 	allocationUpdates["file_meta_root"] = fileMetaRoot
 	allocationUpdates["is_redeem_required"] = true
 
+	if err = db.Model(allocationObj).Updates(allocationUpdates).Error; err != nil {
+		return nil, common.NewError("allocation_write_error", "Error persisting the allocation object")
+	}
+
 	lru := allocation.LRU
 	cachedAllocationInterface, err := lru.Get(allocationId)
 	if err != nil {
@@ -738,9 +742,6 @@ func (fsh *StorageHandler) CommitWrite(ctx context.Context, r *http.Request) (*b
 		return nil, common.NewError("cache_error", "Error adding allocation to cache: "+err.Error())
 	}
 
-	if err = db.Model(allocationObj).Updates(allocationUpdates).Error; err != nil {
-		return nil, common.NewError("allocation_write_error", "Error persisting the allocation object")
-	}
 	err = connectionObj.CommitToFileStore(ctx)
 	if err != nil {
 		if !errors.Is(common.ErrFileWasDeleted, err) {
