3f0f4b089b1d821e460cd9d8afd8dac9b7cc7f3c
dabasov <yuderbasov@gmail.com>
2023-08-05 16:12:00 +0300

    fixed tests

--- code/go/0chain.net/blobbercore/reference/hashnode.go
+++ code/go/0chain.net/blobbercore/reference/hashnode.go
@@ -10,50 +10,43 @@ import (
 
 // LoadRootHashnode load root node with its descendant nodes
 func LoadRootHashnode(ctx context.Context, allocationID string) (*Hashnode, error) {
-	nodes := make(map[string]*Hashnode)
 
-	err := datastore.GetStore().WithNewTransaction(func(ctx context.Context) error {
-		tx := datastore.GetStore().GetTransaction(ctx)
+	tx := datastore.GetStore().GetTransaction(ctx)
 
-		db := tx.Raw(`
-			SELECT allocation_id, type, name, path, validation_root, fixed_merkle_root, actual_file_hash, chunk_size,size,actual_file_size, parent_path
-			FROM reference_objects
-			WHERE allocation_id = ?
-			ORDER BY level desc, path`, allocationID)
+	db := tx.Raw(`
+SELECT allocation_id, type, name, path, validation_root, fixed_merkle_root, actual_file_hash, chunk_size,size,actual_file_size, parent_path
+FROM reference_objects
+WHERE allocation_id = ?
+ORDER BY level desc, path`, allocationID)
 
-		rows, err := db.Rows()
-		if err != nil {
-			return errors.ThrowLog(err.Error(), common.ErrBadDataStore)
-		}
-
-		defer rows.Close()
-
-		for rows.Next() {
-
-			node := &Hashnode{}
-			err = db.ScanRows(rows, node)
-			if err != nil {
-				return errors.ThrowLog(err.Error(), common.ErrBadDataStore)
-			}
-
-			_, ok := nodes[node.Path]
-			if ok {
-				return common.ErrDuplicatedNode
-			}
-
-			nodes[node.Path] = node
-
-			parent, ok := nodes[node.ParentPath]
-			if ok {
-				parent.AddChild(node)
-			}
-
-		}
-		return nil
-	})
-
+	rows, err := db.Rows()
 	if err != nil {
-		return nil, err
+		return nil, errors.ThrowLog(err.Error(), common.ErrBadDataStore)
+	}
+
+	defer rows.Close()
+
+	nodes := make(map[string]*Hashnode)
+	for rows.Next() {
+
+		node := &Hashnode{}
+		err = db.ScanRows(rows, node)
+		if err != nil {
+			return nil, errors.ThrowLog(err.Error(), common.ErrBadDataStore)
+		}
+
+		_, ok := nodes[node.Path]
+		if ok {
+			return nil, common.ErrDuplicatedNode
+		}
+
+		nodes[node.Path] = node
+
+		parent, ok := nodes[node.ParentPath]
+		if ok {
+			parent.AddChild(node)
+		}
+
 	}
 
 	// create empty dir if root is missing
