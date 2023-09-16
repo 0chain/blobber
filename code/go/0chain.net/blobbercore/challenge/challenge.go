7c79e8aac69e2287fa871409429fce97e2de4c5c
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-09-16 09:00:19 +0530

    Increase time limit

--- code/go/0chain.net/blobbercore/challenge/challenge.go
+++ code/go/0chain.net/blobbercore/challenge/challenge.go
@@ -36,7 +36,7 @@ func syncOpenChallenges(ctx context.Context) {
 	params := make(map[string]string)
 	params["blobber"] = node.Self.ID
 
-	params["limit"] = "50"
+	params["limit"] = "30"
 	if lastChallengeRound > 0 {
 		params["from"] = strconv.FormatInt(lastChallengeRound, 10)
 	}
