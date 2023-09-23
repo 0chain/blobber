d4bda04be78a2816f6d0a20edce2544df56b0d0a
dabasov <yuderbasov@gmail.com>
2023-09-23 20:21:59 +0300

    added tx logging

--- code/go/0chain.net/blobbercore/datastore/store.go
+++ code/go/0chain.net/blobbercore/datastore/store.go
@@ -2,7 +2,13 @@ package datastore
 
 import (
 	"context"
+	"runtime/debug"
+	"sync"
+	"time"
 
+	"github.com/0chain/blobber/code/go/0chain.net/core/logging"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
@@ -13,12 +19,65 @@ const (
 	ContextKeyStore
 )
 
+var (
+	txs   map[string]holder
+	guard sync.Mutex
+)
+
+type holder struct {
+	stack   string
+	created int64
+}
+
+func init() {
+	txs = make(map[string]holder)
+
+	go func() {
+		tick := time.Tick(30 * time.Second)
+
+		for {
+			select {
+			case <-tick:
+				guard.Lock()
+				t := txs
+				guard.Unlock()
+				for _, v := range t {
+					if time.Now().UnixNano()-v.created > 30*time.Second.Nanoseconds() {
+						logging.Logger.Error("stale_transaction", zap.String("stack", v.stack))
+					}
+				}
+			}
+		}
+	}()
+}
+
 type EnhancedDB struct {
+	id           string
 	SessionCache map[string]interface{}
 	*gorm.DB
 }
 
+func (e *EnhancedDB) Commit() *gorm.DB {
+	guard.Lock()
+	delete(txs, e.id)
+	guard.Unlock()
+
+	return e.DB.Commit()
+}
+func (e *EnhancedDB) Rollback() *gorm.DB {
+	guard.Lock()
+	delete(txs, e.id)
+	guard.Unlock()
+
+	return e.DB.Rollback()
+}
+
 func EnhanceDB(db *gorm.DB) *EnhancedDB {
+	newUUID, _ := uuid.NewUUID()
+	guard.Lock()
+	txs[newUUID.String()] = holder{string(debug.Stack()), time.Now().UnixNano()}
+	guard.Unlock()
+
 	cache := make(map[string]interface{})
 	return &EnhancedDB{DB: db, SessionCache: cache}
 }
