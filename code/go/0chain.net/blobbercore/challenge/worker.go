e0d62422b47daaf520c90cbee6a4e4301f3eecea
Jayash Satolia <73050737+Jayashsatolia403@users.noreply.github.com>
2023-10-04 19:10:17 +0530

    Fix challenge timing issue (#1277)
    * Fix challenge timing issue
    
    * Fix challenge timing issue
    
    * Fix challenge timing issue

--- code/go/0chain.net/blobbercore/challenge/worker.go
+++ code/go/0chain.net/blobbercore/challenge/worker.go
@@ -50,7 +50,7 @@ var (
 	roundInfo          = RoundInfo{}
 )
 
-const batchSize = 5
+const batchSize = 10
 
 // SetupWorkers start challenge workers
 func SetupWorkers(ctx context.Context) {
@@ -217,16 +217,23 @@ func getBatch(batchSize int) (chall []ChallengeEntity) {
 		return
 	}
 
+	// create a set of string
+	allocations := make(map[string]bool)
+
 	it := challengeMap.Iterator()
 	for it.Next() {
 		if len(chall) >= batchSize {
 			break
 		}
+
 		ticket := it.Value().(*ChallengeEntity)
-		if ticket.Status != Processed {
-			break
+
+		_, ok := allocations[ticket.AllocationID]
+		if ticket.Status != Processed || ok {
+			continue
 		}
 		chall = append(chall, *ticket)
+		allocations[ticket.AllocationID] = true
 	}
 	return
 }
