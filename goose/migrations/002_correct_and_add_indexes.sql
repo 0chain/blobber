c3ba34639f7f484c91fd89e3a41b95f87e86282c
din-mukhammed <dimash.r53@gmail.com>
2023-10-07 23:29:16 +0600

    fix goose migration tags

--- goose/migrations/002_correct_and_add_indexes.sql
+++ goose/migrations/002_correct_and_add_indexes.sql
@@ -1,7 +1,7 @@
--- Up
+-- +goose Up
 
 -- Dropping incorrect index on the 'name' column
-DROP INDEX "idx_name_gin:gin";
+DROP INDEX IF EXISTS "idx_name_gin:gin";
 
 -- Creating a new GIN index for full-text search on the 'name' column
 CREATE INDEX idx_name_gin ON public.reference_objects 
@@ -10,8 +10,7 @@ USING gin(to_tsvector('english', name));
 -- Creating a new GIN index for trigram matching on the 'path' column
 CREATE INDEX idx_path_gin_trgm ON public.reference_objects USING gin(path gin_trgm_ops);
 
--- Down
-
-DROP INDEX idx_path_gin_trgm;
-DROP INDEX idx_name_gin;
-CREATE INDEX "idx_name_gin:gin" ON public.reference_objects USING btree (name);
+-- +goose Down
+DROP INDEX IF EXISTS idx_path_gin_trgm;
+DROP INDEX IF EXISTS idx_name_gin;
+CREATE INDEX IF NOT EXISTS "idx_name_gin:gin" ON public.reference_objects USING btree (name);
