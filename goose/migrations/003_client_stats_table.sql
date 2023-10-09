723e37f96b1f1d241ec457a7416eaddaf921459b
Hitenjain14 <hitenjain146@gmail.com>
2023-10-10 03:31:30 +0530

    fix goose migration

--- goose/migrations/003_client_stats_table.sql
+++ goose/migrations/003_client_stats_table.sql
@@ -1,5 +1,8 @@
 -- +goose Up
--- +goose StatementBegin
+
+--
+-- Name: client_stats; Type: TABLE; Schema: public; Owner: blobber_user
+--
 
 CREATE TABLE public.client_stats(
     client_id character varying(64),
@@ -9,9 +12,13 @@ CREATE TABLE public.client_stats(
     total_write_marker bigint DEFAULT 0 NOT NULL
 )
 
+-- +goose StatementBegin
+
 ALTER TABLE public.client_stats OWNER TO blobber_user;
 
 ALTER TABLE ONLY public.client_stats
     ADD CONSTRAINT client_stats_pkey PRIMARY KEY (client_id, created_at);
-
+--
+-- PostgreSQL database dump complete
+--
 -- +goose StatementEnd
