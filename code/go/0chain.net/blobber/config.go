c6964be8172446b01c2cd102abcc44c939fe751f
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2023-10-10 22:54:45 +0530

    Cleanup worker for challenge tables (#1278)
    * add cleanup worker for challenge tables
    
    * move cleanupGap to config
    
    ---------
    
    Co-authored-by: Yury <yuderbasov@gmail.com>

--- code/go/0chain.net/blobber/config.go
+++ code/go/0chain.net/blobber/config.go
@@ -49,6 +49,7 @@ func setupConfig(configDir string, deploymentMode int) {
 	config.Configuration.ChallengeResolveFreq = viper.GetInt64("challenge_response.frequency")
 	config.Configuration.ChallengeResolveNumWorkers = viper.GetInt("challenge_response.num_workers")
 	config.Configuration.ChallengeMaxRetires = viper.GetInt("challenge_response.max_retries")
+	config.Configuration.ChallengeCleanupGap = viper.GetInt64("challenge_response.cleanup_gap")
 
 	config.Configuration.AutomaticUpdate = viper.GetBool("disk_update.automatic_update")
 	blobberUpdateIntrv := viper.GetDuration("disk_update.blobber_update_interval")
