b09092e7f841fb4aa208ae35c446d2441b6f3f11
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-11-07 22:49:45 +0200

    debug logs

--- code/go/0chain.net/blobbercore/filestore/tree_validation_integration_tests.go
+++ code/go/0chain.net/blobbercore/filestore/tree_validation_integration_tests.go
@@ -14,11 +14,25 @@ import (
 func (v *validationTreeProof) GetMerkleProofOfMultipleIndexes(r io.ReadSeeker, nodesSize int64, startInd, endInd int) (
 	[][][]byte, [][]int, error) {
 	nodes, indexes, err := v.getMerkleProofOfMultipleIndexes(r, nodesSize, startInd, endInd)
+	if err != nil {
+		return nodes, indexes, err
+	}
 
 	state := crpc.Client().State()
 	if state == nil {
 		return nodes, indexes, err
 	}
+
+	logging.Logger.Debug("[conductor] just after getMerkleProofOfMultipleIndexes",
+		zap.Bool("miss_up_download", state.MissUpDownload),
+		zap.Int("start_ind", startInd),
+		zap.Int("end_ind", endInd),
+		zap.Int64("nodes_size", nodesSize),
+		zap.Int("output_nodes_size", len(nodes)),
+		zap.Int("output_indexes_size", len(indexes)),
+		zap.Any("nodes", nodes),
+		zap.Any("indexes", indexes),
+	)
 
 	if state.MissUpDownload {
 		logging.Logger.Debug("miss up/download",
