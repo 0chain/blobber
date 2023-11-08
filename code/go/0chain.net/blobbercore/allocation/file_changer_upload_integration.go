259c0cd6016eddb09910cf71250d18799f11ae75
Jayash Satolia <73050737+Jayashsatolia403@users.noreply.github.com>
2023-11-08 16:25:30 +0530

    Added changes for fail upload commit (#1318)

---
+++ code/go/0chain.net/blobbercore/allocation/file_changer_upload_integration.go
@@ -0,0 +1,28 @@
+//go:build integration_tests
+// +build integration_tests
+
+package allocation
+
+import (
+	"context"
+	"errors"
+
+	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/reference"
+	"github.com/0chain/blobber/code/go/0chain.net/conductor/conductrpc"
+	"github.com/0chain/blobber/code/go/0chain.net/core/common"
+	"github.com/0chain/blobber/code/go/0chain.net/core/node"
+)
+
+func (nf *UploadFileChanger) ApplyChange(ctx context.Context, rootRef *reference.Ref, change *AllocationChange,
+	allocationRoot string, ts common.Timestamp, fileIDMeta map[string]string) (*reference.Ref, error) {
+
+	state := conductrpc.Client().State()
+	if state.FailUploadCommit != nil {
+		for _, nodeId := range state.FailUploadCommit {
+			if nodeId == node.Self.ID {
+				return nil, errors.New("error directed by conductor")
+			}
+		}
+	}
+	return nf.applyChange(ctx, rootRef, change, allocationRoot, ts, fileIDMeta)
+}
