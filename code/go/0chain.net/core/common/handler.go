54b743318ec3eb13d842285f289faa19b4ee5b76
Hitenjain14 <hitenjain146@gmail.com>
2023-11-08 22:23:08 +0530

    rmv content length

--- code/go/0chain.net/core/common/handler.go
+++ code/go/0chain.net/core/common/handler.go
@@ -79,7 +79,6 @@ func ToByteStream(handler JSONResponderF) ReqRespHandlerf {
 			rawdata, ok := data.([]byte)
 			if ok {
 				w.Header().Set("Content-Type", "application/octet-stream")
-				w.Header().Set("Content-Length", fmt.Sprintf("%v", len(rawdata)))
 				w.Write(rawdata) //nolint:errcheck
 			} else {
 				w.Header().Set("Content-Type", "application/json")
@@ -88,7 +87,6 @@ func ToByteStream(handler JSONResponderF) ReqRespHandlerf {
 					http.Error(w, err.Error(), 400)
 					return
 				}
-				w.Header().Set("Content-Length", fmt.Sprintf("%v", len(byteData)))
 				w.Write(byteData) //nolint:errcheck
 			}
 		}
