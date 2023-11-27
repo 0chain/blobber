d01e4ca8d0afcf927e6d76e3848175a7ed1fd6aa
Hitenjain14 <hitenjain146@gmail.com>
2023-11-28 00:54:33 +0530

    rmv write_locks table

--- code/go/0chain.net/blobbercore/writemarker/write_lock.go
+++ code/go/0chain.net/blobbercore/writemarker/write_lock.go
@@ -2,17 +2,8 @@ package writemarker
 
 import "time"
 
-const (
-	TableNameWriteLock = "write_locks"
-)
-
 // WriteLock WriteMarker lock
 type WriteLock struct {
 	ConnectionID string    `gorm:"column:connection_id;size:64"`
 	CreatedAt    time.Time `gorm:"column:created_at"`
-}
-
-// TableName get table name of migrate
-func (WriteLock) TableName() string {
-	return TableNameWriteLock
 }
