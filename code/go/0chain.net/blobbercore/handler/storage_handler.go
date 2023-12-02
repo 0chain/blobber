48e86b878ff58e530677ad745acc50aa64bf5d4f
Hitenjain14 <hitenjain146@gmail.com>
2023-12-03 00:57:33 +0530

    fix log

--- code/go/0chain.net/blobbercore/handler/storage_handler.go
+++ code/go/0chain.net/blobbercore/handler/storage_handler.go
@@ -416,10 +416,10 @@ func (fsh *StorageHandler) GetLatestWriteMarker(ctx context.Context, r *http.Req
 
 	var latestWM *writemarker.WriteMarkerEntity
 	var prevWM *writemarker.WriteMarkerEntity
-	Logger.Info("[latest_write_marker]", zap.String("allocation_root", allocationObj.AllocationRoot), zap.String("allocation_id", allocationObj.ID))
 	if allocationObj.AllocationRoot == "" {
 		latestWM = nil
 	} else {
+		Logger.Info("[latest_write_marker]", zap.String("allocation_root", allocationObj.AllocationRoot), zap.String("allocation_id", allocationObj.ID))
 		latestWM, err = writemarker.GetWriteMarkerEntity(ctx, allocationObj.AllocationRoot)
 		if err != nil {
 			return nil, common.NewError("latest_write_marker_read_error", "Error reading the latest write marker for allocation. "+err.Error())
