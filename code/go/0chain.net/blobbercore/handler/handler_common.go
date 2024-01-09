476cb4c07408e83f98eda37a827c91a525c52a5b
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2024-01-10 01:54:29 +0530

    fix commit lock (#1365)

--- code/go/0chain.net/blobbercore/handler/handler_common.go
+++ code/go/0chain.net/blobbercore/handler/handler_common.go
@@ -125,8 +125,8 @@ func WithStatusConnectionForWM(handler common.StatusCodeResponderF) common.Statu
 		}
 
 		// Lock will compete with other CommitWrites and Challenge validation
-		var allocationObj *allocation.Allocation
-		mutex := lock.GetMutex(allocationObj.TableName(), allocationID)
+
+		mutex := lock.GetMutex(allocation.Allocation{}.TableName(), allocationID)
 		Logger.Info("Locking allocation", zap.String("allocation_id", allocationID))
 		mutex.Lock()
 		defer mutex.Unlock()
