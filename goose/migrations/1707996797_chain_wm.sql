1d5d87e833317fc349fc7e68b0bbd56db8772403
Hitenjain14 <hitenjain146@gmail.com>
2024-02-15 17:04:10 +0530

    add sql migration

---
+++ goose/migrations/1707996797_chain_wm.sql
@@ -0,0 +1,7 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE write_markers
+ADD COLUMN chain_hash character varying(64);
+ADD COLUMN chain_size BIGINT;
+ADD COLUMN chain_length integer;
+-- +goose StatementEnd
