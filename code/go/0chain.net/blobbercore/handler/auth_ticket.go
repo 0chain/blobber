e748b0b319838a8bd06f382c3c4b10ddfa445caa
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-25 04:20:52 +0530

    Debug

--- code/go/0chain.net/blobbercore/handler/auth_ticket.go
+++ code/go/0chain.net/blobbercore/handler/auth_ticket.go
@@ -4,16 +4,10 @@ import (
 	"context"
 	"fmt"
 	"github.com/0chain/blobber/code/go/0chain.net/core/node"
-	"github.com/0chain/common/core/logging"
-	"go.uber.org/zap"
 	"net/http"
 )
 
 func GenerateAuthTicket(ctx context.Context, r *http.Request) (interface{}, error) {
-	logging.Logger.Info("GenerateAuthTicket")
-	logging.Logger.Info("1GenerateAuthTicket")
-	logging.Logger.Info("2GenerateAuthTicket", zap.Any("query", r.URL.Query()))
 	clientID := r.URL.Query().Get("client_id")
-	logging.Logger.Info("GenerateAuthTicket", zap.Any("client_id", clientID))
 	return node.Self.Sign(fmt.Sprintf("%s:%s", node.Self.ID, clientID))
 }
