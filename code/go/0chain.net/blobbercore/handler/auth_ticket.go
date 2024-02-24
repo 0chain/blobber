9e555dec058f12118436a3c310e7c24990bded68
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-25 00:40:50 +0530

    Debug

--- code/go/0chain.net/blobbercore/handler/auth_ticket.go
+++ code/go/0chain.net/blobbercore/handler/auth_ticket.go
@@ -13,6 +13,8 @@ import (
 )
 
 func GenerateAuthTicket(ctx context.Context, r *http.Request) (interface{}, error) {
+	logging.Logger.Info("GenerateAuthTicket")
+	logging.Logger.Info("0GenerateAuthTicket", zap.Any("node_id", node.ID()))
 	logging.Logger.Info("1GenerateAuthTicket", zap.Any("client_id", r.URL.Query()), zap.Any("node_id", node.ID()), zap.Any("public_key", node.PublicKey()))
 	logging.Logger.Info("2GenerateAuthTicket", zap.Any("private_key", node.PrivateKey()), zap.Any("signature_scheme", config.Configuration.SignatureScheme))
 
