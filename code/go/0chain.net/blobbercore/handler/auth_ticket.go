365b9b40aac189ea8194d2c4339f33817d568c91
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-25 02:17:27 +0530

    Debug

--- code/go/0chain.net/blobbercore/handler/auth_ticket.go
+++ code/go/0chain.net/blobbercore/handler/auth_ticket.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/0chain/blobber/code/go/0chain.net/core/config"
 	"github.com/0chain/blobber/code/go/0chain.net/core/logging"
+	node2 "github.com/0chain/blobber/code/go/0chain.net/core/node"
 	"github.com/0chain/gosdk/core/zcncrypto"
 	"github.com/0chain/gosdk/zmagmacore/node"
 	"go.uber.org/zap"
@@ -13,11 +14,12 @@ import (
 )
 
 func GenerateAuthTicket(ctx context.Context, r *http.Request) (interface{}, error) {
+
 	logging.Logger.Info("GenerateAuthTicket")
-	logging.Logger.Info("1GenerateAuthTicket", zap.Any("public_key", node.PublicKey()))
-	logging.Logger.Info("2GenerateAuthTicket", zap.Any("private_key", node.PrivateKey()))
+	selfNode := node2.GetSelfNode(ctx)
+	logging.Logger.Info("1GenerateAuthTicket", zap.Any("public_key", selfNode))
+	logging.Logger.Info("2GenerateAuthTicket", zap.Any("public_key", selfNode.GetWallet()))
 	logging.Logger.Info("3GenerateAuthTicket", zap.Any("client_id", r.URL.Query()))
-	logging.Logger.Info("4GenerateAuthTicket", zap.Any("id", node.ID()))
 
 	clientID := r.URL.Query().Get("client_id")
 	ticket := fmt.Sprintf("%s:%s", node.ID(), clientID)
