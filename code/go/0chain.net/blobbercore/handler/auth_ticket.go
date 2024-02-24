7e03984e128cfc5d4f39a87f2fa47be5a23e6529
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-25 00:13:27 +0530

    Fix import cycle

--- code/go/0chain.net/blobbercore/handler/auth_ticket.go
+++ code/go/0chain.net/blobbercore/handler/auth_ticket.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/0chain/blobber/code/go/0chain.net/core/config"
-	"github.com/0chain/common/core/logging"
+	"github.com/0chain/blobber/code/go/0chain.net/core/logging"
 	"github.com/0chain/gosdk/core/zcncrypto"
 	"github.com/0chain/gosdk/zmagmacore/node"
 	"go.uber.org/zap"
@@ -13,17 +13,16 @@ import (
 )
 
 func GenerateAuthTicket(ctx context.Context, r *http.Request) (interface{}, error) {
-	logging.Logger.Info("Jayash GenerateAuthTicket")
+	logging.Logger.Info("1GenerateAuthTicket", zap.Any("client_id", r.URL.Query()), zap.Any("node_id", node.ID()), zap.Any("public_key", node.PublicKey()))
+	logging.Logger.Info("2GenerateAuthTicket", zap.Any("private_key", node.PrivateKey()), zap.Any("signature_scheme", config.Configuration.SignatureScheme))
+
 	clientID := r.URL.Query().Get("client_id")
-	logging.Logger.Info("Jayash GenerateAuthTicket", zap.Any("client_id", clientID))
 	ticket := fmt.Sprintf("%s:%s", node.ID(), clientID)
-	logging.Logger.Info("Jayash GenerateAuthTicket", zap.Any("ticket", ticket))
 
 	signatureScheme := zcncrypto.NewSignatureScheme(config.Configuration.SignatureScheme)
 	_ = signatureScheme.SetPrivateKey(node.PrivateKey())
 	_ = signatureScheme.SetPublicKey(node.PublicKey())
 
 	res, err := signatureScheme.Sign(hex.EncodeToString([]byte(ticket)))
-	logging.Logger.Info("Jayash GenerateAuthTicket", zap.Any("res", res), zap.Any("err", err))
 	return res, err
 }
