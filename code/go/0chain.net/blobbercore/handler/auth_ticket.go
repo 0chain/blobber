721cfd3be3a4f9788072d0ad2289db7061de8dc9
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-25 15:53:27 +0530

    Debug

--- code/go/0chain.net/blobbercore/handler/auth_ticket.go
+++ code/go/0chain.net/blobbercore/handler/auth_ticket.go
@@ -2,12 +2,19 @@ package handler
 
 import (
 	"context"
-	"fmt"
+	"github.com/0chain/blobber/code/go/0chain.net/core/logging"
 	"github.com/0chain/blobber/code/go/0chain.net/core/node"
+	"github.com/0chain/common/core/common"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 func GenerateAuthTicket(ctx context.Context, r *http.Request) (interface{}, error) {
+	logging.Logger.Info("GenerateAuthTicket", zap.Any("query", r.URL.Query()))
 	clientID := r.URL.Query().Get("client_id")
-	return node.Self.Sign(fmt.Sprintf("%s:%s", node.Self.ID, clientID))
+	if clientID == "" {
+		return nil, common.NewError("missing_client_id", "client_id is required")
+	}
+	logging.Logger.Info("GenerateAuthTicket", zap.Any("client_id", clientID))
+	return node.Self.Sign(clientID)
 }
