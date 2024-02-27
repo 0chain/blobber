c69a1f841c1c1797b437a98f77cbcc431a61c232
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-28 01:34:58 +0530

    Added blobber authorization feature

---
+++ code/go/0chain.net/blobbercore/handler/auth_ticket.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"context"
+	"github.com/0chain/blobber/code/go/0chain.net/core/logging"
+	"github.com/0chain/blobber/code/go/0chain.net/core/node"
+	"github.com/0chain/common/core/common"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+func GenerateAuthTicket(ctx context.Context, r *http.Request) (interface{}, error) {
+	logging.Logger.Info("GenerateAuthTicket", zap.Any("query", r.URL.Query()))
+	clientID := r.URL.Query().Get("client_id")
+	if clientID == "" {
+		return nil, common.NewError("missing_client_id", "client_id is required")
+	}
+	logging.Logger.Info("GenerateAuthTicket", zap.Any("client_id", clientID))
+	logging.Logger.Info("GenerateAuthTicket", zap.Any("node", node.Self))
+	logging.Logger.Info("GenerateAuthTicket", zap.Any("node", node.Self.GetWallet()))
+	logging.Logger.Info("GenerateAuthTicket", zap.Any("node", node.Self.GetWalletString()))
+	return node.Self.Sign(clientID)
+}
