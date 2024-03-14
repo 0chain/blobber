38ca3e009b61d3c735855a958357a1e48ba6df8e
Hitenjain14 <hitenjain146@gmail.com>
2024-03-14 19:33:07 +0530

    add filestore version log

--- goose/migrations/1710413946_filestore_version.sql
+++ goose/migrations/1710413946_filestore_version.sql
@@ -1,4 +1,4 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE reference_objects ADD COLUMN filestore_version INTEGER;
+ALTER TABLE reference_objects ADD COLUMN filestore_version INTEGER DEFAULT 0;
 -- +goose StatementEnd
