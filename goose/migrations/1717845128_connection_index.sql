4bcfed050e35df3a89276712d1a7ea796e7d253b
Hitenjain14 <hitenjain146@gmail.com>
2024-06-08 20:58:25 +0530

    fix test

--- goose/migrations/1717845128_connection_index.sql
+++ goose/migrations/1717845128_connection_index.sql
@@ -3,5 +3,4 @@
 
 ALTER TABLE allocation_changes DROP CONSTRAINT allocation_changes_pkey CASCADE,
 ADD CONSTRAINT allocation_changes_pkey PRIMARY KEY (lookup_hash);
-ALTER TABLE allocation_changes DROP COLUMN id;
 -- +goose StatementEnd
