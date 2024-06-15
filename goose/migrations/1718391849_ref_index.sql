98e71844497827d6b527d96b13388710d8eeb93f
Hitenjain14 <hitenjain146@gmail.com>
2024-06-15 12:24:50 +0530

    add allocation path and level index

--- goose/migrations/1718391849_ref_index.sql
+++ goose/migrations/1718391849_ref_index.sql
@@ -1,5 +1,5 @@
 -- +goose Up
 -- +goose StatementBegin
-DROP INDEX idx_created_at,idx_updated_at,idx_lookup_hash,path_idx;
-CREATE UNIQUE INDEX idx_lookup_deleted_at ON reference_objects USING btree(lookup_hash,deleted_at);
+DROP INDEX idx_created_at,idx_updated_at,idx_lookup_hash,path_idx,idx_path_gin_trgm,idx_name_gin,idx_path_alloc;
+CREATE INDEX idx_allocation_path ON reference_objects USING btree(allocation_id,path,level);
 -- +goose StatementEnd
