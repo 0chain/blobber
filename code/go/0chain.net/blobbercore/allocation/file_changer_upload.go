01c209b7a71a4eff48f910e525f21ee7894b7b7c
Hitenjain14 <hitenjain146@gmail.com>
2024-07-31 00:51:59 +0530

    remove lock in upload changer

--- code/go/0chain.net/blobbercore/allocation/file_changer_upload.go
+++ code/go/0chain.net/blobbercore/allocation/file_changer_upload.go
@@ -101,16 +101,13 @@ func (nf *UploadFileChanger) applyChange(ctx context.Context,
 	// get parent id
 	parent := filepath.Dir(nf.Path)
 	// create or get parent directory
-	collector.LockTransaction()
 	parentRef, err := reference.Mkdir(ctx, nf.AllocationID, parent, allocationVersion, ts, collector)
 	if err != nil {
-		collector.UnlockTransaction()
 		return err
 	}
 	elapsedMkdir := time.Since(now) - elapsedNewFileRecord - elapsedNewFile
 	newFile.ParentID = &parentRef.ID
 	collector.CreateRefRecord(newFile)
-	collector.UnlockTransaction()
 	elapsedCreateRefRecord := time.Since(now) - elapsedMkdir - elapsedNewFileRecord - elapsedNewFile
 	logging.Logger.Info("UploadFileChanger", zap.Duration("elapsedNewFile", elapsedNewFile), zap.Duration("elapsedNewFileRecord", elapsedNewFileRecord), zap.Duration("elapsedMkdir", elapsedMkdir), zap.Duration("elapsedCreateRefRecord", elapsedCreateRefRecord), zap.Duration("elapsedTotal", time.Since(now)))
 	return err
