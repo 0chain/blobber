0c5abaa2e27c993eefc7c43b7875ef59fad3d2eb
Hitenjain14 <hitenjain146@gmail.com>
2024-07-31 16:34:39 +0530

    remove index from drop

--- goose/migrations/1718391849_ref_index.sql
+++ goose/migrations/1718391849_ref_index.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-DROP INDEX idx_created_at,idx_updated_at,idx_path_gin_trgm,idx_name_gin,idx_allocation_changes_lookup_hash;
+DROP INDEX idx_created_at,idx_updated_at,idx_path_gin_trgm,idx_name_gin;
 
 CREATE INDEX idx_is_allocation_version_deleted_at on reference_objects(allocation_id,allocation_version) INCLUDE(lookup_hash,id) WHERE type='f' AND deleted_at IS NULL;
 
