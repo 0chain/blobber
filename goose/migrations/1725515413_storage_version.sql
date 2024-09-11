e2db13312b234b00fa1e6d4453498f5b46b17fb4
hitenjain14 <hitenjain146@gmail.com>
2024-09-11 15:10:03 +0530

    fix col name

--- goose/migrations/1725515413_storage_version.sql
+++ goose/migrations/1725515413_storage_version.sql
@@ -24,7 +24,7 @@ CREATE INDEX idx_path_alloc ON reference_objects USING btree (allocation_id, pat
 
 CREATE INDEX idx_is_deleted ON reference_objects(allocation_id) WHERE deleted_at is NOT NULL;
 
-CREATE INDEX idx_is_allocation_root_deleted_at ON reference_objects(allocation_id, is_allocation_root) WHERE type='f' AND deleted_at IS NULL;
+CREATE INDEX idx_is_allocation_root_deleted_at ON reference_objects(allocation_id, allocation_root) WHERE type='f' AND deleted_at IS NULL;
 
 CREATE INDEX idx_path_alloc_level ON reference_objects USING btree (allocation_id,level,type,path) WHERE deleted_at IS NULL;
 
