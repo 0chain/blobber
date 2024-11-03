d07f67346265a521b62626af3082f118df3bac63
Hitenjain14 <hitenjain146@gmail.com>
2024-11-03 23:40:17 +0530

    set to read only in commit v2

--- code/go/0chain.net/blobbercore/handler/object_operation_handler.go
+++ code/go/0chain.net/blobbercore/handler/object_operation_handler.go
@@ -912,7 +912,7 @@ func (fsh *StorageHandler) CommitWriteV2(ctx context.Context, r *http.Request) (
 		return nil, common.NewError("blacklisted_client", "Client is blacklisted: "+clientID)
 	}
 
-	allocationObj, err := fsh.verifyAllocation(ctx, allocationId, allocationTx, false)
+	allocationObj, err := fsh.verifyAllocation(ctx, allocationId, allocationTx, true)
 	if err != nil {
 		return nil, common.NewError("invalid_parameters", "Invalid allocation id passed."+err.Error())
 	}
