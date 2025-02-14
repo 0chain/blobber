746b20855607a3f35ca8500819bc9c5ef11af1ce
smaulik13 <mauliksinghal4@gmail.com>
2025-02-14 19:27:10 +0530

    logging

--- code/go/0chain.net/blobbercore/handler/storage_handler.go
+++ code/go/0chain.net/blobbercore/handler/storage_handler.go
@@ -520,7 +520,7 @@ func (fsh *StorageHandler) GetLatestWriteMarker(ctx context.Context, r *http.Req
 	clientSignV2 := ctx.Value(constants.ContextKeyClientSignatureHeaderV2Key).(string)
 	valid, err := verifySignatureFromRequest(allocationTx, clientSign, clientSignV2, publicKey)
 	if !valid || err != nil {
-		return nil, common.NewError("invalid_signature", "could not verify the allocation owner")
+		return nil, common.NewError("invalid_signature", "could not verify the allocation owner"+err.Error())
 	}
 
 	var vm *writemarker.VersionMarker
