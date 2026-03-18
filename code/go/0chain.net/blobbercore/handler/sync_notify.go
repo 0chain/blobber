1b54659a2c1a5707c27da6441011db7486ac70c5
Nahom Amare <nahomkifle925@gmail.com>
2026-03-18 19:41:32 +0300

    fix: remove the logger and update the 0chain_blobber.yaml file

--- code/go/0chain.net/blobbercore/handler/sync_notify.go
+++ code/go/0chain.net/blobbercore/handler/sync_notify.go
@@ -4,9 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/0chain/blobber/code/go/0chain.net/blobbercore/config"
-	. "github.com/0chain/blobber/code/go/0chain.net/core/logging"
 	"github.com/0chain/blobber/code/go/0chain.net/core/util"
-	"go.uber.org/zap"
 )
 
 func notifySyncEvent(allocationID, clientID, eventType string) {
@@ -14,34 +12,16 @@ func notifySyncEvent(allocationID, clientID, eventType string) {
 		return
 	}
 
-	Logger.Info("sync_notify_queued",
-		zap.String("allocation_id", allocationID),
-		zap.String("client_id", clientID),
-		zap.String("event_type", eventType),
-		zap.String("notify_url", config.Configuration.SyncNotifyURL))
-
 	payload, err := json.Marshal(map[string]string{
 		"allocation_id": allocationID,
 		"client_id":     clientID,
 		"event_type":    eventType,
 	})
 	if err != nil {
-		Logger.Error("sync_notify_marshal_failed", zap.Error(err))
 		return
 	}
 
 	go func() {
-		resp, err := util.SendPostRequest(config.Configuration.SyncNotifyURL, payload, nil)
-		if err != nil {
-			Logger.Error("sync_notify_failed",
-				zap.String("allocation_id", allocationID),
-				zap.String("event_type", eventType),
-				zap.Error(err))
-		} else {
-			Logger.Info("sync_notify_sent",
-				zap.String("allocation_id", allocationID),
-				zap.String("event_type", eventType),
-				zap.String("response", string(resp)))
-		}
+		util.SendPostRequest(config.Configuration.SyncNotifyURL, payload, nil)
 	}()
 }
